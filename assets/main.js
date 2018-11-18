eb11d5852594f2c5b000f6ff44103c67b70ed521
Amir Raminfar <findamir@gmail.com>
2018-11-18 11:09:33 -0800

    Adds new layout and uses sass to get bulma

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,11 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "./App.vue";
-import Index from "./pages/Index.vue";
 import Container from "./pages/Container.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: "/", component: Index },
   {
     path: "/container/:id",
     component: Container,
