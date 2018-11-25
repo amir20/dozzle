5979a6d0e5f9c119364d816b1133f3b2476b84ad
Amir Raminfar <findamir@gmail.com>
2018-11-24 17:32:22 -0800

    Adds responsiveness for menu on left

--- assets/App.vue
+++ assets/App.vue
@@ -1,9 +1,17 @@
 <template lang="html">
   <div class="columns is-marginless">
-    <aside class="column menu is-2 section">
-      <h1 class="title has-text-warning">Dozzle</h1>
-      <p class="menu-label">Containers</p>
-      <ul class="menu-list">
+    <aside class="column menu is-2">
+      <a
+        role="button"
+        class="navbar-burger burger is-white is-hidden-tablet is-pulled-right"
+        @click="showNav = !showNav;"
+        :class="{ 'is-active': showNav }"
+      >
+        <span></span> <span></span> <span></span>
+      </a>
+      <h1 class="title has-text-warning is-marginless">Dozzle</h1>
+      <p class="menu-label is-hidden-mobile" :class="{ 'is-active': showNav }">Containers</p>
+      <ul class="menu-list is-hidden-mobile" :class="{ 'is-active': showNav }">
         <li v-for="item in containers">
           <router-link
             :to="{ name: 'container', params: { id: item.Id } }"
@@ -25,7 +33,8 @@ export default {
   name: "App",
   data() {
     return {
-      containers: []
+      containers: [],
+      showNav: false
     };
   },
   async created() {
@@ -34,15 +43,45 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.is-hidden-mobile.is-active {
+  display: block !important;
+}
+
+.navbar-burger {
+  height: 2.35rem;
+}
+
 aside {
   position: fixed;
-  padding-right: 0;
+  z-index: 2;
+  padding: 1em;
+
+  @media screen and (max-width: 768px) {
+    & {
+      position: sticky;
+      top: 0;
+      background: #222;
+    }
+
+    .tooltip::after,
+    .tooltip::before {
+      display: none !important;
+    }
+
+    .menu-label {
+      margin-top: 1em;
+    }
+  }
 }
 
 .hide-overflow {
   text-overflow: ellipsis;
   white-space: nowrap;
   overflow: hidden;
+}
+
+.burger.is-white {
+  color: #fff;
 }
 </style>
