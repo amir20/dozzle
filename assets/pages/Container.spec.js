9e2135d92308c0df96d7f2e195eb3ddeadd33192
Amir <amirraminfar@google.com>
2019-06-13 11:21:52 -0700

    Adds more tests

--- assets/pages/Container.spec.js
+++ assets/pages/Container.spec.js
@@ -1,6 +1,7 @@
 import EventSource from "eventsourcemock";
 import { sources } from "eventsourcemock";
 import { shallowMount } from "@vue/test-utils";
+import MockDate from "mockdate";
 import Container from "./Container";
 
 describe("<Container />", () => {
@@ -8,6 +9,8 @@ describe("<Container />", () => {
     global.BASE_PATH = "";
     global.EventSource = EventSource;
   });
+
+  afterEach(() => MockDate.reset());
 
   test("is a Vue instance", async () => {
     const wrapper = shallowMount(Container);
@@ -37,19 +40,36 @@ describe("<Container />", () => {
   });
 
   test("should parse messages", async () => {
+    MockDate.set("6/12/2019");
     const wrapper = shallowMount(Container, {
       propsData: { id: "abc" }
     });
     sources["/api/logs/stream?id=abc"].emitOpen();
     sources["/api/logs/stream?id=abc"].emitMessage({ data: `2019-06-13T00:55:42.459034602Z "This is a message."` });
-    const [{ dateRelative, ...other }, _] = wrapper.vm.messages;
+    const [message, _] = wrapper.vm.messages;
 
-    expect(other).toMatchInlineSnapshot(`
-      Object {
-        "date": 2019-06-13T00:55:42.459Z,
-        "key": 0,
-        "message": " \\"This is a message.\\"",
-      }
+    expect(message).toMatchInlineSnapshot(`
+            Object {
+              "date": 2019-06-13T00:55:42.459Z,
+              "dateRelative": "today at 5:55 PM",
+              "key": 0,
+              "message": " \\"This is a message.\\"",
+            }
+        `);
+  });
+
+  test("should render messages", async () => {
+    MockDate.set("6/12/2019");
+    const wrapper = shallowMount(Container, {
+      propsData: { id: "abc" }
+    });
+    sources["/api/logs/stream?id=abc"].emitOpen();
+    sources["/api/logs/stream?id=abc"].emitMessage({ data: `2019-06-13T00:55:42.459034602Z "This is a message."` });
+
+    expect(wrapper.find("ul.events")).toMatchInlineSnapshot(`
+      <ul class="events">
+        <li class="event"><span class="date">today at 5:55 PM</span> <span class="text"> "This is a message."</span></li>
+      </ul>
     `);
   });
 });
