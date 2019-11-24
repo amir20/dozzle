29075715228662be583df41dfd3bc7db3c61a99e
Amir <amirraminfar@google.com>
2019-11-24 11:48:40 -0800

    Adds vuex for component

--- assets/components/LogEventSource.spec.js
+++ assets/components/LogEventSource.spec.js
@@ -1,6 +1,7 @@
 import EventSource from "eventsourcemock";
 import { sources } from "eventsourcemock";
 import { shallowMount, mount, createLocalVue } from "@vue/test-utils";
+import Vuex from "vuex";
 import MockDate from "mockdate";
 import LogEventSource from "./LogEventSource.vue";
 import LogViewer from "./LogViewer.vue";
@@ -15,13 +16,22 @@ describe("<LogEventSource />", () => {
 
   afterEach(() => MockDate.reset());
 
-  function createLogEventSource() {
+  function createLogEventSource(searchFilter = null) {
     const localVue = createLocalVue();
+    localVue.use(Vuex);
+
     localVue.component("log-event-source", LogEventSource);
     localVue.component("log-viewer", LogViewer);
 
+    const state = { searchFilter };
+
+    const store = new Vuex.Store({
+      state
+    });
+
     return mount(LogEventSource, {
       localVue,
+      store,
       scopedSlots: {
         default: `
         <log-viewer :messages="props.messages"></log-viewer>
@@ -48,14 +58,14 @@ describe("<LogEventSource />", () => {
   });
 
   test("should close EventSource", async () => {
-    const wrapper = createLogEventSource(LogEventSource);
+    const wrapper = createLogEventSource();
     sources["/api/logs/stream?id=abc"].emitOpen();
     wrapper.destroy();
     expect(sources["/api/logs/stream?id=abc"].readyState).toBe(2);
   });
 
   test("should parse messages", async () => {
-    const wrapper = createLogEventSource(LogEventSource);
+    const wrapper = createLogEventSource();
     sources["/api/logs/stream?id=abc"].emitOpen();
     sources["/api/logs/stream?id=abc"].emitMessage({ data: `2019-06-12T10:55:42.459034602Z "This is a message."` });
     const [message, _] = wrapper.vm.messages;
@@ -72,7 +82,7 @@ describe("<LogEventSource />", () => {
   });
 
   test("should pass messages to slot", async () => {
-    const wrapper = createLogEventSource(LogEventSource);
+    const wrapper = createLogEventSource();
     sources["/api/logs/stream?id=abc"].emitOpen();
     sources["/api/logs/stream?id=abc"].emitMessage({ data: `2019-06-12T10:55:42.459034602Z "This is a message."` });
     const [message, _] = wrapper.find(LogViewer).vm.messages;
@@ -90,7 +100,7 @@ describe("<LogEventSource />", () => {
   });
 
   test("should render messages", async () => {
-    const wrapper = createLogEventSource(LogEventSource);
+    const wrapper = createLogEventSource();
     sources["/api/logs/stream?id=abc"].emitOpen();
     sources["/api/logs/stream?id=abc"].emitMessage({ data: `2019-06-12T10:55:42.459034602Z "This is a message."` });
 
@@ -102,7 +112,7 @@ describe("<LogEventSource />", () => {
   });
 
   test("should render messages with color", async () => {
-    const wrapper = createLogEventSource(LogEventSource);
+    const wrapper = createLogEventSource();
     sources["/api/logs/stream?id=abc"].emitOpen();
     sources["/api/logs/stream?id=abc"].emitMessage({
       data: `2019-06-12T10:55:42.459034602Z \x1b[30mblack\x1b[37mwhite`
@@ -116,7 +126,7 @@ describe("<LogEventSource />", () => {
   });
 
   test("should render messages with html entities", async () => {
-    const wrapper = createLogEventSource(LogEventSource);
+    const wrapper = createLogEventSource();
     sources["/api/logs/stream?id=abc"].emitOpen();
     sources["/api/logs/stream?id=abc"].emitMessage({
       data: `2019-06-12T10:55:42.459034602Z <test>foo bar</test>`
@@ -130,7 +140,7 @@ describe("<LogEventSource />", () => {
   });
 
   test("should render messages with filter", async () => {
-    const wrapper = createLogEventSource(LogEventSource);
+    const wrapper = createLogEventSource("test");
     sources["/api/logs/stream?id=abc"].emitOpen();
     sources["/api/logs/stream?id=abc"].emitMessage({
       data: `2019-06-11T10:55:42.459034602Z Foo bar`
@@ -138,8 +148,6 @@ describe("<LogEventSource />", () => {
     sources["/api/logs/stream?id=abc"].emitMessage({
       data: `2019-06-12T10:55:42.459034602Z This is a test <hi></hi>`
     });
-
-    wrapper.find(LogViewer).setData({ filter: "test" });
 
     expect(wrapper.find("ul.events")).toMatchInlineSnapshot(`
       <ul class="events">
