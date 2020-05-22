237032a71420f789d68176e687df0af89632e3b3
Amir Raminfar <findamir@gmail.com>
2020-05-22 10:36:59 -0700

    Removes mocked date

--- assets/components/LogEventSource.spec.js
+++ assets/components/LogEventSource.spec.js
@@ -3,7 +3,6 @@ import EventSource from "eventsourcemock";
 import { sources } from "eventsourcemock";
 import { shallowMount, mount, createLocalVue } from "@vue/test-utils";
 import Vuex from "vuex";
-import MockDate from "mockdate";
 import LogEventSource from "./LogEventSource.vue";
 import LogViewer from "./LogViewer.vue";
 
@@ -18,9 +17,7 @@ jest.mock("../store/config.js", () => ({ base: "" }));
 describe("<LogEventSource />", () => {
   beforeEach(() => {
     global.EventSource = EventSource;
-    MockDate.set("6/12/2019", 0);
     window.scrollTo = jest.fn();
-
     const observe = jest.fn();
     const unobserve = jest.fn();
     global.IntersectionObserver = jest.fn(() => ({
@@ -29,8 +26,6 @@ describe("<LogEventSource />", () => {
     }));
     debounce.mockClear();
   });
-
-  afterEach(() => MockDate.reset());
 
   function createLogEventSource(searchFilter = null) {
     const localVue = createLocalVue();
@@ -126,64 +121,76 @@ describe("<LogEventSource />", () => {
     `);
   });
 
-  test("should render messages", async () => {
-    const wrapper = createLogEventSource();
-    sources["/api/logs/stream?id=abc"].emitOpen();
-    sources["/api/logs/stream?id=abc"].emitMessage({ data: `2019-06-12T10:55:42.459034602Z "This is a message."` });
+  describe("render html correctly", () => {
+    const RealDate = Date;
+    beforeAll(() => {
+      global.Date = class extends RealDate {
+        constructor() {
+          return new RealDate("2019-06-12T10:55:42.459034602Z");
+        }
+      };
+    });
+    afterAll(() => (global.Date = RealDate));
 
-    await wrapper.vm.$nextTick();
-    expect(wrapper.find("ul.events")).toMatchInlineSnapshot(`
-      <ul class="events medium">
-        <li class=""><span class="date">today at 10:55 AM</span> <span class="text">"This is a message."</span></li>
-      </ul>
-    `);
-  });
+    test("should render messages", async () => {
+      const wrapper = createLogEventSource();
+      sources["/api/logs/stream?id=abc"].emitOpen();
+      sources["/api/logs/stream?id=abc"].emitMessage({ data: `2019-06-12T10:55:42.459034602Z "This is a message."` });
 
-  test("should render messages with color", async () => {
-    const wrapper = createLogEventSource();
-    sources["/api/logs/stream?id=abc"].emitOpen();
-    sources["/api/logs/stream?id=abc"].emitMessage({
-      data: `2019-06-12T10:55:42.459034602Z \x1b[30mblack\x1b[37mwhite`,
+      await wrapper.vm.$nextTick();
+      expect(wrapper.find("ul.events")).toMatchInlineSnapshot(`
+        <ul class="events medium">
+          <li class=""><span class="date">today at 10:55 AM</span> <span class="text">"This is a message."</span></li>
+        </ul>
+      `);
     });
 
-    await wrapper.vm.$nextTick();
-    expect(wrapper.find("ul.events")).toMatchInlineSnapshot(`
-      <ul class="events medium">
-        <li class=""><span class="date">today at 10:55 AM</span> <span class="text"><span style="color:#000">black<span style="color:#AAA">white</span></span></span></li>
-      </ul>
-    `);
-  });
+    test("should render messages with color", async () => {
+      const wrapper = createLogEventSource();
+      sources["/api/logs/stream?id=abc"].emitOpen();
+      sources["/api/logs/stream?id=abc"].emitMessage({
+        data: `2019-06-12T10:55:42.459034602Z \x1b[30mblack\x1b[37mwhite`,
+      });
 
-  test("should render messages with html entities", async () => {
-    const wrapper = createLogEventSource();
-    sources["/api/logs/stream?id=abc"].emitOpen();
-    sources["/api/logs/stream?id=abc"].emitMessage({
-      data: `2019-06-12T10:55:42.459034602Z <test>foo bar</test>`,
+      await wrapper.vm.$nextTick();
+      expect(wrapper.find("ul.events")).toMatchInlineSnapshot(`
+        <ul class="events medium">
+          <li class=""><span class="date">today at 10:55 AM</span> <span class="text"><span style="color:#000">black<span style="color:#AAA">white</span></span></span></li>
+        </ul>
+      `);
     });
 
-    await wrapper.vm.$nextTick();
-    expect(wrapper.find("ul.events")).toMatchInlineSnapshot(`
-      <ul class="events medium">
-        <li class=""><span class="date">today at 10:55 AM</span> <span class="text">&lt;test&gt;foo bar&lt;/test&gt;</span></li>
-      </ul>
-    `);
-  });
+    test("should render messages with html entities", async () => {
+      const wrapper = createLogEventSource();
+      sources["/api/logs/stream?id=abc"].emitOpen();
+      sources["/api/logs/stream?id=abc"].emitMessage({
+        data: `2019-06-12T10:55:42.459034602Z <test>foo bar</test>`,
+      });
 
-  test("should render messages with filter", async () => {
-    const wrapper = createLogEventSource("test");
-    sources["/api/logs/stream?id=abc"].emitOpen();
-    sources["/api/logs/stream?id=abc"].emitMessage({
-      data: `2019-06-11T10:55:42.459034602Z Foo bar`,
-    });
-    sources["/api/logs/stream?id=abc"].emitMessage({
-      data: `2019-06-12T10:55:42.459034602Z This is a test <hi></hi>`,
+      await wrapper.vm.$nextTick();
+      expect(wrapper.find("ul.events")).toMatchInlineSnapshot(`
+        <ul class="events medium">
+          <li class=""><span class="date">today at 10:55 AM</span> <span class="text">&lt;test&gt;foo bar&lt;/test&gt;</span></li>
+        </ul>
+      `);
     });
 
-    await wrapper.vm.$nextTick();
-    expect(wrapper.find("ul.events")).toMatchInlineSnapshot(`
-      <ul class="events medium">
-        <li class=""><span class="date">today at 10:55 AM</span> <span class="text">This is a <mark>test</mark> &lt;hi&gt;&lt;/hi&gt;</span></li>
-      </ul>
-    `);
+    test("should render messages with filter", async () => {
+      const wrapper = createLogEventSource("test");
+      sources["/api/logs/stream?id=abc"].emitOpen();
+      sources["/api/logs/stream?id=abc"].emitMessage({
+        data: `2019-06-11T10:55:42.459034602Z Foo bar`,
+      });
+      sources["/api/logs/stream?id=abc"].emitMessage({
+        data: `2019-06-12T10:55:42.459034602Z This is a test <hi></hi>`,
+      });
+
+      await wrapper.vm.$nextTick();
+      expect(wrapper.find("ul.events")).toMatchInlineSnapshot(`
+        <ul class="events medium">
+          <li class=""><span class="date">today at 10:55 AM</span> <span class="text">This is a <mark>test</mark> &lt;hi&gt;&lt;/hi&gt;</span></li>
+        </ul>
+      `);
+    });
   });
 });
