bae3ecca7b62a0fc8a1a822c980bd2b3067b1994
Amir Raminfar <findamir@gmail.com>
2020-05-22 16:49:23 -0700

    Adds show page with name param (#474)

---
+++ assets/pages/Show.vue
@@ -0,0 +1,24 @@
+<template lang="html"> </template>
+
+<script>
+import { mapActions, mapGetters, mapState } from "vuex";
+export default {
+  props: [],
+  name: "Show",
+  computed: mapGetters(["visibleContainers"]),
+  watch: {
+    visibleContainers(newValue) {
+      if (newValue) {
+        if (this.$route.query.name) {
+          const [container, _] = this.visibleContainers.filter((c) => c.name.includes(this.$route.query.name));
+          this.$router.push({ name: "container", params: { id: container.id } });
+        } else {
+          console.error(`Expection query parameter name to be set. Redirecting to /`);
+          this.$router.push({ name: "default" });
+        }
+      }
+    },
+  },
+};
+</script>
+<style scoped></style>
