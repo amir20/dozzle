af60119d1a91bc40a51611794862cd3c259a7c37
Amir Raminfar <findamir@gmail.com>
2020-06-20 15:38:50 -0700

    Fixes animation

--- assets/components/ScrollProgress.vue
+++ assets/components/ScrollProgress.vue
@@ -3,27 +3,54 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
 import throttle from "lodash.throttle";
+import debounce from "lodash.debounce";
 
 export default {
   name: "ScrollProgress",
   data() {
     return {
       scrollProgress: 0,
+      animation: { cancel: () => {} },
+      parentElement: document,
     };
   },
   mounted() {
-    const onScroll = throttle(() => {
-      const p = document.documentElement;
+    this.attachEvents();
+    this.$once("hook:beforeDestroy", this.detachEvents);
+  },
+  watch: {
+    activeContainers() {
+      this.detachEvents();
+      this.attachEvents();
+    },
+  },
+  computed: {
+    ...mapState(["activeContainers"]),
+  },
+  methods: {
+    attachEvents() {
+      this.parentElement = this.$el.closest("[data-scrolling]") || document;
+      this.parentElement.addEventListener("scroll", this.onScroll);
+    },
+    detachEvents() {
+      this.parentElement.removeEventListener("scroll", this.onScroll);
+    },
+    onScroll: throttle(function () {
+      const p = this.parentElement == document ? document.documentElement : this.parentElement;
       this.scrollProgress = p.scrollTop / (p.scrollHeight - p.clientHeight);
-      this.$refs.progress.animate([{ opacity: 1 }, { opacity: 0 }], {
-        duration: 300,
-        fill: "both",
-        delay: 2000,
-      });
-    }, 150);
-    document.addEventListener("scroll", onScroll);
-    this.$once("hook:beforeDestroy", () => document.removeEventListener("scroll", onScroll));
+      this.animation.cancel();
+      this.animation = this.$refs.progress.animate(
+        { opacity: [1, 0] },
+        {
+          duration: 500,
+          delay: 2000,
+          fill: "both",
+          easing: "ease-out",
+        }
+      );
+    }, 150),
   },
 };
 </script>
@@ -32,10 +59,11 @@ export default {
   background: #00d1b2;
   background-repeat: no-repeat;
   position: fixed;
-  width: 100%;
   height: 4px;
-  z-index: 1;
+  z-index: 2;
   left: 0;
+  right: 0;
+  top: 0;
   transform: scaleX(var(--progress));
   transform-origin: left;
   transition: transform 0.3s ease;
