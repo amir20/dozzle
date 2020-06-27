4cd2a81291ee4a6b85377ecc76665801ad2d4183
Amir Raminfar <findamir@gmail.com>
2020-06-27 11:03:20 -0700

    Fixes search

--- assets/components/Search.vue
+++ assets/components/Search.vue
@@ -75,11 +75,12 @@ export default {
   width: 350px;
   position: fixed;
   padding: 10px;
-  background: rgba(50, 50, 50, 0.9);
+  background: var(--scheme-main-ter);
   top: 0;
   right: 0;
   border-radius: 0 0 0 5px;
   z-index: 10;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
 }
 .delete {
   margin-left: 1em;
