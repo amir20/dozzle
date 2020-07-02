08cc04379d1c6bff990b7f1b626ddfc3c8e1fa40
Amir Raminfar <findamir@gmail.com>
2020-07-02 11:32:37 -0700

    Adds hot reload for development (#553)

--- assets/store/config.js
+++ assets/store/config.js
@@ -1,2 +1,6 @@
 const config = JSON.parse(document.querySelector("script#config__json").textContent);
+if (config.version == "{{ .Version }}") {
+  config.version = "dev";
+  config.base = "";
+}
 export default config;
