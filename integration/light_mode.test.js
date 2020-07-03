5302ace41346df4859eca302854297546f421144
Amir Raminfar <findamir@gmail.com>
2020-07-03 15:16:01 -0700

    Adds more tests for light mode

---
+++ integration/light_mode.test.js
@@ -0,0 +1,37 @@
+const puppeteer = require("puppeteer");
+const iPhoneX = puppeteer.devices["iPhone X"];
+const iPadLandscape = puppeteer.devices["iPad landscape"];
+
+const { DEFAULT_URL: URL } = process.env;
+
+describe("Dozzle with light mode", () => {
+  beforeAll(async () => {
+    await page.goto(URL + "/settings", { waitUntil: "networkidle2" });
+    await page.$$eval("label.switch", (elements) => {
+      elements.filter((e) => e.textContent.trim() === "Use light theme")[0].click();
+    });
+  });
+  beforeEach(async () => {
+    await page.goto(URL, { waitUntil: "networkidle2" });
+  });
+
+  it("renders full page on desktop", async () => {
+    const image = await page.screenshot({ fullPage: true });
+
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it("renders ipad viewport", async () => {
+    await page.emulate(iPadLandscape);
+    const image = await page.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it("renders iphone viewport", async () => {
+    await page.emulate(iPhoneX);
+    const image = await page.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  });
+});
