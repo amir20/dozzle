46db2b035ed534feb18a869d36e50b6f3c827ea0
Amir Raminfar <findamir@gmail.com>
2020-07-09 13:03:46 -0700

    Fixes tests

--- assets/pages/Index.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from "@vue/test-utils";
-import Index from "./Index";
-
-describe("<Index />", () => {
-  test("renders correctly", () => {
-    const wrapper = shallowMount(Index);
-    expect(wrapper.element).toMatchSnapshot();
-  });
-});
