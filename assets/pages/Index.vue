b25de1d921194284f367615c8c1b71211781c8f8
Amir Raminfar <findamir@gmail.com>
2020-07-10 11:06:28 -0700

    Adds a new dashboard for default page (#569)

--- assets/pages/Index.vue
+++ assets/pages/Index.vue
@@ -4,11 +4,8 @@
       <div class="hero-body">
         <div class="container">
           <h1 class="title">
-            Hello, friend.
+            Hello, there!
           </h1>
-          <h2 class="subtitle">
-            I hope you are having a great day!
-          </h2>
         </div>
       </div>
     </section>
@@ -16,36 +13,59 @@
       <div class="level-item has-text-centered">
         <div>
           <p class="title">{{ containers.length }}</p>
-          <p class="heading">Containers</p>
+          <p class="heading">Total Containers</p>
         </div>
       </div>
       <div class="level-item has-text-centered">
         <div>
-          <p class="title">{{ visibleContainers.length }}</p>
+          <p class="title">{{ runningContainers.length }}</p>
           <p class="heading">Running</p>
         </div>
       </div>
       <div class="level-item has-text-centered">
         <div>
-          <p class="title">1.24.1</p>
+          <p class="title">{{ version }}</p>
           <p class="heading">Dozzle Version</p>
         </div>
       </div>
     </section>
 
-    <section class="columns is-centered section">
+    <section class="columns is-centered">
       <div class="column is-4">
         <div class="panel">
           <p class="panel-heading">
-            Recent Containers
+            Containers
+          </p>
+          <div class="panel-block">
+            <p class="control has-icons-left">
+              <input
+                class="input"
+                type="text"
+                placeholder="Search Containers"
+                v-model="search"
+                @keyup.esc="search = null"
+              />
+              <span class="icon is-left">
+                <icon name="search"></icon>
+              </span>
+            </p>
+          </div>
+          <p class="panel-tabs">
+            <a :class="{ 'is-active': sort === 'recent' }" @click="sort = 'recent'">Recent</a>
+            <a :class="{ 'is-active': sort === 'running' }" @click="sort = 'running'">Running</a>
+            <a :class="{ 'is-active': sort === 'all' }" @click="sort = 'all'">All</a>
           </p>
           <router-link
             :to="{ name: 'container', params: { id: item.id, name: item.name } }"
-            v-for="item in topMostRecent"
+            v-for="item in results.slice(0, 10)"
             :key="item.id"
             class="panel-block"
           >
-            {{ item.name }}
+            <span class="name">{{ item.name }}</span>
+
+            <!-- <div class="subtitle is-7 status">
+              {{ item.status }}
+            </div> -->
           </router-link>
         </div>
       </div>
@@ -55,14 +75,46 @@
 
 <script>
 import { mapActions, mapGetters, mapState } from "vuex";
+import Icon from "../components/Icon";
+import config from "../store/config";
+
 export default {
   name: "Index",
+  components: { Icon },
+  data() {
+    return {
+      version: config.version,
+      search: null,
+      sort: "recent",
+    };
+  },
 
   computed: {
-    ...mapGetters(["visibleContainers"]),
     ...mapState(["containers"]),
-    topMostRecent() {
-      return this.visibleContainers.sort((a, b) => b.created - a.created).slice(0, 10);
+    mostRecentContainers() {
+      return [...this.containers].sort((a, b) => b.created - a.created);
+    },
+    runningContainers() {
+      return this.containers.filter((c) => c.state === "running");
+    },
+    allContainers() {
+      return this.containers;
+    },
+    results() {
+      if (this.search) {
+        const term = this.search.toLowerCase();
+        return this.allContainers.filter((c) => c.name.toLowerCase().includes(term));
+      }
+      switch (this.sort) {
+        case "all":
+          return this.allContainers;
+        case "running":
+          return this.runningContainers;
+        case "recent":
+          return this.mostRecentContainers;
+        default:
+          throw `Invalid sort order: ${this.sort}`;
+      }
     },
   },
 };
@@ -70,8 +122,25 @@ export default {
 <style lang="scss" scoped>
 .panel {
   border: 1px solid var(--border-color);
-  .panel-block {
+  .panel-block,
+  .panel-tabs {
     border-color: var(--border-color);
+    .is-active {
+      border-color: var(--border-hover-color);
+    }
+    .name {
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      overflow: hidden;
+    }
+    .status {
+      margin-left: auto;
+      white-space: nowrap;
+    }
   }
+}
+
+.icon {
+  padding: 10px 3px;
 }
 </style>
