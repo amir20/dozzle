e86e92d366a874abaced01e232caf8dd6313709b
dima-bzz <dimabzz@gmail.com>
2020-11-25 17:35:14 +0300

    Updated time format. (#846)
    * Added choice of clock format to settings. Updated babelrc.
    
    Signed-off-by: Dmitry Mazurov <dimabzz@gmail.com>
    
    * Updated time format checker to support old and new browsers.
    
    Signed-off-by: Dmitry Mazurov <dimabzz@gmail.com>

--- assets/components/RelativeTime.vue
+++ assets/components/RelativeTime.vue
@@ -1,8 +1,9 @@
 <template>
-  <time :datetime="date.toISOString()">{{ date | relativeTime }}</time>
+  <time :datetime="date.toISOString()">{{ date | relativeTime(locale) }}</time>
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex";
 import { formatRelative } from "date-fns";
 import { enGB, enUS } from "date-fns/locale";
 
@@ -10,10 +11,12 @@ const use24Hr =
   new Intl.DateTimeFormat(undefined, {
     hour: "numeric",
   })
-    .formatToParts(new Date(2020, 0, 1, 13))
-    .find((part) => part.type === "hour").value.length === 2;
+    .format(new Date(2020, 0, 1, 13))
+    .replace(/[^0-9]/g, '')
+    .length === 2;
 
-const locale = use24Hr ? enGB : enUS;
+const auto = use24Hr ? enGB : enUS;
+const styles = { auto, 12: enUS, 24: enGB };
 
 export default {
   props: {
@@ -24,9 +27,14 @@ export default {
   },
   name: "RelativeTime",
   components: {},
-
+  computed: {
+    ...mapState(["settings"]),
+    locale () {
+      return styles[this.settings.hourStyle];
+    }
+  },
   filters: {
-    relativeTime(date) {
+    relativeTime(date, locale) {
       return formatRelative(date, new Date(), { locale });
     },
   },
