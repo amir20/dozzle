eeec34b01848ac5e205f0febb5cb30cbc48a0f4a
Amir Raminfar <findamir@gmail.com>
2021-10-29 14:26:50 -0700

    Removes @sha...

--- assets/components/ContainerTitle.vue
+++ assets/components/ContainerTitle.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="columns is-marginless has-text-weight-bold is-family-monospace">
     <span class="column is-ellipsis"
-      >{{ container.name }} <span class="tag is-dark">{{ container.image }}</span></span
+      >{{ container.name }} <span class="tag is-dark">{{ container.image.replace(/@sha.*/, "") }}</span></span
     >
   </div>
 </template>
