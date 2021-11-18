6d3693b1ff2b7450db4355ad11ab91f8f7cc31c9
Amir Raminfar <findamir@gmail.com>
2021-11-18 13:29:34 -0800

    Fixes tests for storage

--- assets/composables/settings.ts
+++ assets/composables/settings.ts
@@ -1,9 +1,18 @@
-import { computed, WritableComputedRef } from "vue";
 import { useStorage } from "@vueuse/core";
+import { computed } from "vue";
 
 export const DOZZLE_SETTINGS_KEY = "DOZZLE_SETTINGS";
 
-export const DEFAULT_SETTINGS = {
+export const DEFAULT_SETTINGS: {
+  search: boolean;
+  size: "small" | "medium" | "large";
+  menuWidth: number;
+  smallerScrollbars: boolean;
+  showTimestamp: boolean;
+  showAllContainers: boolean;
+  lightTheme: boolean;
+  hourStyle: "auto" | "24" | "12";
+} = {
   search: true,
   size: "medium",
   menuWidth: 15,
--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  testPathIgnorePatterns: ["node_modules", "<rootDir>/integration/", "<rootDir>/e2e/"],
-  transform: {
-    "^.+\\.vue$": "@vue/vue3-jest",
-  },
-};
