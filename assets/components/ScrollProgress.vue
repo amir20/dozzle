6cfb42412cd2e5742b1659d7c50f8aa04f2eb749
Amir Raminfar <findamir@gmail.com>
2022-01-20 14:23:43 -0800

    Uses useScroll instead of handling the scroll event myself (#1642)
    * Tries to use-scroll instead of scroll listener
    
    * Moves observer to scrollable content only
    
    * Fixes bugs

--- assets/components/ScrollProgress.vue
+++ assets/components/ScrollProgress.vue
@@ -19,9 +19,9 @@
 
 <script lang="ts" setup>
 import { useContainerStore } from "@/stores/container";
-import throttle from "lodash.throttle";
+import { useScroll } from "@vueuse/core";
 import { storeToRefs } from "pinia";
-import { onMounted, onUnmounted, ref, watchPostEffect } from "vue";
+import { onMounted, ref, watch, watchPostEffect } from "vue";
 
 const props = defineProps({
   indeterminate: {
@@ -36,15 +36,28 @@ const props = defineProps({
 
 const scrollProgress = ref(0);
 const animation = ref({ cancel: () => {} });
-const parentElement = ref<Node>(document);
 const root = ref<HTMLElement>();
 const store = useContainerStore();
 const { activeContainers } = storeToRefs(store);
-const onScrollThrottled = throttle(onScroll, 150);
+const scrollElement = ref<HTMLElement | Document>((root.value?.closest("[data-scrolling]") as HTMLElement) ?? document);
+const { y: scrollY } = useScroll(scrollElement, { throttle: 100 });
 
-function onScroll() {
-  const parent = parentElement.value == document ? document.documentElement : (parentElement.value as HTMLElement);
-  scrollProgress.value = parent.scrollTop / (parent.scrollHeight - parent.clientHeight);
+onMounted(() => {
+  watch(
+    activeContainers,
+    () => {
+      scrollElement.value = (root.value?.closest("[data-scrolling]") as HTMLElement) ?? document;
+    },
+    { immediate: true, flush: "post" }
+  );
+});
+
+watchPostEffect(() => {
+  const parent =
+    scrollElement.value === document
+      ? (scrollElement.value as Document).documentElement
+      : (scrollElement.value as HTMLElement);
+  scrollProgress.value = scrollY.value / (parent.scrollHeight - parent.clientHeight);
   animation.value.cancel();
   if (props.autoHide && root.value) {
     animation.value = root.value.animate(
@@ -57,29 +70,6 @@ function onScroll() {
       }
     );
   }
-}
-
-function attachEvents() {
-  parentElement.value = root.value?.closest("[data-scrolling]") || document;
-  parentElement.value.addEventListener("scroll", onScrollThrottled);
-}
-
-function detachEvents() {
-  parentElement.value.removeEventListener("scroll", onScrollThrottled);
-}
-
-onMounted(() => {
-  attachEvents();
-});
-
-onUnmounted(() => {
-  detachEvents();
-});
-
-watchPostEffect(() => {
-  activeContainers.value.length;
-  detachEvents();
-  attachEvents();
 });
 </script>
 <style scoped lang="scss">
