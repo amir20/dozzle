6ef3da9abd0ac4976c8f8b0ba1962e58204c7e3e
Amir Raminfar <findamir@gmail.com>
2022-08-15 13:00:49 -0700

    Adds dark mode

--- e2e/cypress/e2e/dozzle_dark.cy.js
+++ e2e/cypress/e2e/dozzle_dark.cy.js
@@ -1,20 +1,12 @@
 /// <reference types="cypress" />
 
-context.skip("Dozzle dark mode", { baseUrl: Cypress.env("DOZZLE_DEFAULT") }, () => {
+context("Dozzle dark mode", { baseUrl: Cypress.env("DOZZLE_DEFAULT") }, () => {
   beforeEach(() => {
-    Cypress.on("window:before:load", (win) => {
-      cy.stub(win, "matchMedia")
-        .withArgs("(prefers-color-scheme: dark)")
-        .returns({
-          matches: true,
-        })
-        .as("dark-media-query");
-    });
-
-    cy.visit("/", {});
+    cy.visit("/");
+    cy.window().then((win) => win.document.documentElement.setAttribute("data-theme", "dark"));
   });
 
   it("home screen", () => {
-    cy.get("li.running", { timeout: 10000 }).removeDates().matchImage();
+    cy.get("li.running", { timeout: 10000 }).removeDates().replaceSkippedElements().matchImage();
   });
 });
