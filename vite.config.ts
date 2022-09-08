860f43862e5a628cc97a18b3b9781e855d9f1594
Amir Raminfar <findamir@gmail.com>
2022-09-07 19:18:21 -0700

    Uses sync mode

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig(({ mode }) => ({
     }),
     Pages({
       dirs: "assets/pages",
+      importMode: "sync",
     }),
     Components({
       dirs: ["assets/components"],
