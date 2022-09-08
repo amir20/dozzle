22dc192da5b6d0331454f1538bb20579333fd3ef
Amir Raminfar <findamir@gmail.com>
2022-09-08 11:57:18 -0700

    Adds asset changes for i18n

---
+++ assets/modules/i18n.ts
@@ -0,0 +1,19 @@
+import { type App } from "vue";
+import { createI18n } from "vue-i18n";
+
+export const install = (app: App) => {
+  const messages = Object.fromEntries(
+    Object.entries(import.meta.glob<{ default: any }>("../../locales/*.y(a)?ml", { eager: true })).map(
+      ([key, value]) => {
+        const yaml = key.endsWith(".yaml");
+        return [key.slice(14, yaml ? -5 : -4), value.default];
+      }
+    )
+  );
+  const i18n = createI18n({
+    legacy: false,
+    locale: "en",
+    messages,
+  });
+  app.use(i18n);
+};
