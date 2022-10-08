fe424ac53574c4fe1f5b7545e5e6cba1cc05cfd5
Amir Raminfar <findamir@gmail.com>
2022-10-08 13:26:07 -0700

    Adds more d3 modules

--- assets/components/CpuSparkline.vue
+++ assets/components/CpuSparkline.vue
@@ -1,6 +1,12 @@
 <template></template>
 
 <script lang="ts" setup>
-import { select } from "d3";
-console.log(select);
+import { select } from "d3-selection";
+import { active, transition } from "d3-transition";
+import { scaleLinear } from "d3-scale";
+import { line } from "d3-shape";
+import { easeLinear } from "d3-ease";
+
+const d3 = { select, active, transition, scaleLinear, line, easeLinear };
+console.log(d3);
 </script>
