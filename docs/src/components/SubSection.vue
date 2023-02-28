3a30bcf937dd4260c6168b29f4b8ce21929fac11
Amir Raminfar <findamir@gmail.com>
2023-02-28 13:42:39 -0800

    Fixes grid

--- docs/src/components/SubSection.vue
+++ docs/src/components/SubSection.vue
@@ -2,11 +2,11 @@
 
 <template>
   <div space-y-20 p="x-4 t-10">
-    <section grid="~ lg:cols-3" gap="y-1 x-6">
+    <section lg:grid-cols-3 gap="y-1 x-6">
       <h2 text="2xl" tracking-tight>
         <slot name="title" />
       </h2>
-      <div grid="lg:col-span-2" prose text-lg max-w-7xl min-w-0>
+      <div grid="lg:col-span-2" prose text-lg max-w-7xl>
         <slot />
       </div>
     </section>
