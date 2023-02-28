beee7ea9f86d5769f43f1d3b6f8ac5eefa210a13
Amir Raminfar <findamir@gmail.com>
2023-02-28 15:51:08 -0800

    Fixes grid bug

--- docs/src/components/SubSection.vue
+++ docs/src/components/SubSection.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div space-y-20 p="x-4 t-10">
-    <section lg:grid-cols-3 gap="y-1 x-6">
+    <section lg:grid lg:grid-cols-3 gap="y-1 x-6">
       <h2 text="2xl" tracking-tight>
         <slot name="title" />
       </h2>
