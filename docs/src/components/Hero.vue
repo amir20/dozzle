01c63a3650a03ae76db2e756bf11455fb37f4695
Amir Raminfar <findamir@gmail.com>
2023-03-08 13:03:36 -0800

    Updates colors and adds button

--- docs/src/components/Hero.vue
+++ docs/src/components/Hero.vue
@@ -4,10 +4,17 @@
   <div p="x-4 lg:y-10" mb-4 text="gray-700 dark:gray-200">
     <div grid="~ lg:cols-3 lg:gap-8" h-full mx-auto>
       <div flex="~ col" text="center" p="x-4">
-        <h1 text-4xl font-light m="t-8 lg:y-auto">
-          <span font-playfair dark:text-brand>Dozzle</span> is a modern log viewer for Docker providing efficient and intuitive ways to
-          view and search logs from containers
-        </h1>
+        <div m="t-8 lg:y-auto">
+          <h1 text-4xl font-light>
+            <span font-playfair dark:text-brand>Dozzle</span> is a modern log viewer for Docker providing efficient and intuitive ways to
+            view and search logs from containers
+          </h1>
+          <div mt-8>
+            <router-link to="/guide/getting-started" btn-primary>
+              Get Started
+            </router-link>
+          </div>
+        </div>
       </div>
       <div grid="lg:col-span-2" flex m="t-8 lg:t-0">
         <div my-auto>
--- docs/unocss.config.ts
+++ docs/unocss.config.ts
@@ -9,12 +9,17 @@ import {
 
 import { presetTypography } from '@unocss/preset-typography'
 import transformerDirectives from '@unocss/transformer-directives'
+import Color from 'color'
+
+const lighten = (c: string, n = 0.1) => Color(c).lighten(n).hex()
+const darken = (c: string, n = 0.1) => Color(c).darken(n).hex()
 
 export default defineConfig({
   shortcuts: [
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600 !outline-none'],
     ['main-bg', 'bg-main-light dark:bg-main-dark'],
+    ['btn-primary', 'rounded-full no-underline py-3 px-4 bg-primary hover:bg-primary-dark text-white hover:text-white text-lg focus:outline-none border-primary-light border-1 border-solid'],
     [/^circle-(\w+)$/, ([, c]) => `rounded-full bg-${c}500 w-3 h-3`],
   ],
   transformers: [
@@ -43,6 +48,11 @@ export default defineConfig({
   theme: {
     colors: {
       brand: 'hsl(44, 100%, 77%)',
+      primary: {
+        DEFAULT: 'hsl(171, 100%, 41%)',
+        light: lighten('hsl(171, 100%, 41%)'),
+        dark: darken('hsl(171, 100%, 41%)'),
+      },
       mainLight: '#eff0eb',
       mainDark: '#1e1e20',
     },
