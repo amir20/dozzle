6fecfce3c7f98cfc5a0be6b18dea68cc7f56b6ee
Amir Raminfar <findamir@gmail.com>
2023-03-21 12:35:32 -0700

    Updates color on video

--- docs/.vitepress/theme/components/BrowserWindow.vue
+++ docs/.vitepress/theme/components/BrowserWindow.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div border="~ solid rounded dark-50" bg="[#121212]">
+  <div border="~ solid rounded dark-50" bg="[#222]">
     <div flex gap="2" p-2>
       <div circle-red />
       <div circle-yellow />
