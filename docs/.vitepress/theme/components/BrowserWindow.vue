07352f6e160a9f1dbf2d95aac2e416465a532855
Amir Raminfar <findamir@gmail.com>
2023-03-21 12:41:22 -0700

    Adds dark and light for browser

--- docs/.vitepress/theme/components/BrowserWindow.vue
+++ docs/.vitepress/theme/components/BrowserWindow.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div border="~ solid rounded dark-50" bg="[#222]">
+  <div border="~ solid rounded light-100 dark:dark-50" bg="[#eee] dark:[#222]">
     <div flex gap="2" p-2>
       <div circle-red />
       <div circle-yellow />
