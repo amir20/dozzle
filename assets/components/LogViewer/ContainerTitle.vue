a33e7d8895bd29dd98429b59e6866f62e5535b9f
Amir Raminfar <findamir@gmail.com>
2023-04-06 09:44:52 -0700

    Adds tooltip to healthcheck. Fixes #2104

--- assets/components/LogViewer/ContainerTitle.vue
+++ assets/components/LogViewer/ContainerTitle.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="columns is-marginless has-text-weight-bold is-family-monospace">
     <span class="column is-ellipsis">
+      <span class="icon is-small health" :health="container.health" v-if="container.health" :title="container.health">
+        <cil:check-circle v-if="container.health == 'healthy'" />
+        <cil:x-circle v-else-if="container.health == 'unhealthy'" />
+        <cil:circle v-else />
+      </span>
       {{ container.name }}
       <span class="tag is-dark">{{ container.image.replace(/@sha.*/, "") }}</span>
     </span>
@@ -14,4 +19,18 @@ import { type ComputedRef } from "vue";
 const container = inject("container") as ComputedRef<Container>;
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.icon {
+  vertical-align: middle;
+}
+
+.health {
+  &[health="unhealthy"] {
+    color: var(--red-color);
+  }
+
+  &[health="healthy"] {
+    color: var(--green-color);
+  }
+}
+</style>
