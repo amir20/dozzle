3c7021af4251e11ba7cad3c52eb9f5525094f624
Amir Raminfar <findamir@gmail.com>
2023-04-27 12:37:36 -0700

    fix: removes unused imports

--- assets/modules/bulma.ts
+++ assets/modules/bulma.ts
@@ -1,16 +1,5 @@
 import { type App } from "vue";
-import {
-  Autocomplete,
-  Button,
-  Dropdown,
-  Switch,
-  Radio,
-  Skeleton,
-  Field,
-  Tooltip,
-  Modal,
-  Config,
-} from "@oruga-ui/oruga-next";
+import { Autocomplete, Button, Dropdown, Switch, Skeleton, Field, Modal, Config } from "@oruga-ui/oruga-next";
 import { bulmaConfig } from "@oruga-ui/theme-bulma";
 
 export const install = (app: App) => {
