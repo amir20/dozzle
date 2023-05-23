721b5dc40ee8d63b414d73d3cf121c6f732cd313
Amir Raminfar <findamir@gmail.com>
2023-05-23 12:06:35 -0700

    feat: shows error message when container is not found

--- assets/pages/container/[id].vue
+++ assets/pages/container/[id].vue
@@ -1,6 +1,11 @@
 <template>
   <search></search>
-  <log-container :id="id" show-title :scrollable="activeContainers.length > 0"> </log-container>
+  <log-container :id="id" show-title :scrollable="activeContainers.length > 0" v-if="currentContainer"></log-container>
+  <div v-else-if="ready" class="notification is-warning is-light m-6">
+    <h1 class="title">
+      {{ $t("error.container-not-found") }}
+    </h1>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -9,7 +14,7 @@ const store = useContainerStore();
 const { id } = defineProps<{ id: string }>();
 
 const currentContainer = store.currentContainer($$(id));
-const { activeContainers } = storeToRefs(store);
+const { activeContainers, ready } = storeToRefs(store);
 
 setTitle("loading");
 
