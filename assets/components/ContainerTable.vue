263c7096d2a406d6b2bdf4905afb2db52d8ae4a0
Amir Raminfar <findamir@gmail.com>
2023-07-31 09:45:27 -0700

    feat: adds sorting to dashboard

--- assets/components/ContainerTable.vue
+++ assets/components/ContainerTable.vue
@@ -2,16 +2,22 @@
 <template>
   <table class="table is-fullwidth">
     <thead>
-      <tr>
-        <th>
-          <a href="" @click.prevent="sortField = 'name'">{{ $t("label.container-name") }}</a>
+      <tr :data-direction="direction > 0 ? 'asc' : 'desc'">
+        <th
+          v-for="(label, field) in headers"
+          :key="field"
+          @click.prevent="sort(field)"
+          :class="{ 'selected-sort': field === sortField }"
+        >
+          <a>
+            <span class="icon-text">
+              <span>{{ $t(label) }}</span>
+              <span class="icon">
+                <mdi:arrow-up />
+              </span>
+            </span>
+          </a>
         </th>
-        <th>{{ $t("label.status") }}</th>
-        <th>{{ $t("label.last-started") }}</th>
-        <th>
-          <a href="" @click.prevent="sortField = 'cpu'">{{ $t("label.avg-cpu") }}</a>
-        </th>
-        <th>{{ $t("label.avg-mem") }}</th>
       </tr>
     </thead>
     <tbody>
@@ -35,13 +41,22 @@
   <nav class="pagination is-right" role="navigation" aria-label="pagination" v-if="isPaginated">
     <ul class="pagination-list">
       <li v-for="i in totalPages">
-        <a class="pagination-link" :class="{ 'is-current': i === currentPage }" @click="currentPage = i">{{ i }}</a>
+        <a class="pagination-link" :class="{ 'is-current': i === currentPage }" @click.prevent="currentPage = i">{{
+          i
+        }}</a>
       </li>
     </ul>
   </nav>
 </template>
 
 <script setup lang="ts">
+const headers = {
+  name: "label.container-name",
+  state: "label.status",
+  created: "label.last-started",
+  cpu: "label.avg-cpu",
+  mem: "label.avg-mem",
+};
 const { containers, perPage = 15 } = defineProps<{
   containers: {
     movingAverage: { cpu: number; memory: number };
@@ -52,23 +67,25 @@ const { containers, perPage = 15 } = defineProps<{
   }[];
   perPage?: number;
 }>();
-const sortField: Ref<"cpu" | "mem" | "name" | "created"> = ref("created");
+const sortField: Ref<keyof typeof headers> = ref("created");
+const direction = ref<1 | -1>(1);
 const sortedContainers = computedWithControl(
-  () => [containers.length, sortField.value],
+  () => [containers.length, sortField.value, direction.value],
   () => {
     console.log("sorting");
     return containers.sort((a, b) => {
       if (sortField.value === "name") {
-        return a.name.localeCompare(b.name);
+        return direction.value * a.name.localeCompare(b.name);
       } else if (sortField.value === "created") {
-        return a.created.getTime() - b.created.getTime();
+        return direction.value * (a.created.getTime() - b.created.getTime());
       } else if (sortField.value === "cpu") {
-        return a.movingAverage.cpu - b.movingAverage.cpu;
+        return direction.value * (a.movingAverage.cpu - b.movingAverage.cpu);
       } else if (sortField.value === "mem") {
-        return a.movingAverage.memory - b.movingAverage.memory;
-      } else {
-        throw new Error("Unknown sort field");
+        return direction.value * (a.movingAverage.memory - b.movingAverage.memory);
+      } else if (sortField.value === "state") {
+        return direction.value * a.state.localeCompare(b.state);
       }
+      throw new Error("Invalid sort field");
     });
   },
 );
@@ -82,6 +99,30 @@ const paginated = computed(() => {
 
   return sortedContainers.value.slice(start, end);
 });
+
+function sort(field: keyof typeof headers) {
+  if (sortField.value === field) {
+    direction.value *= -1;
+  } else {
+    sortField.value = field;
+    direction.value = 1;
+  }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.icon {
+  display: none;
+  transition: transform 0.2s ease-in-out;
+  [data-direction="desc"] & {
+    transform: rotate(180deg);
+  }
+}
+.selected-sort {
+  font-weight: bold;
+  border-color: var(--primary);
+  .icon {
+    display: inline-block;
+  }
+}
+</style>
