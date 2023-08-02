ac886b0e9aa03736ff0760bb57b5f90ff57777f3
Amir Raminfar <findamir@gmail.com>
2023-08-02 11:29:50 -0700

    feat: adds bar charts back to dashboard table (#2331)

--- assets/components/BarChart.vue
+++ assets/components/BarChart.vue
@@ -15,10 +15,17 @@ const minValue = computed(() => Math.min(value, 1));
 
 <style scoped>
 .bar {
-  height: 100%;
+  height: 1.5em;
   background-color: var(--primary-color);
   transform-origin: left;
   transform: scaleX(v-bind(minValue));
   transition: transform 0.2s ease-out;
+  border-top-right-radius: 0.2em;
+  border-bottom-right-radius: 0.2em;
+}
+
+.is-overlay {
+  font-size: 0.9em;
+  padding: 0 0.5em;
 }
 </style>
