2cfcfc994b41b76c6800ff820dfb947d0368e137
Amir Raminfar <findamir@gmail.com>
2023-09-07 11:44:51 -0700

    removes table component

--- assets/modules/oruga.ts
+++ assets/modules/oruga.ts
@@ -1,7 +1,7 @@
 import { type App } from "vue";
-import { Autocomplete, Skeleton, Field, Table, Modal, Config } from "@oruga-ui/oruga-next";
+import { Autocomplete, Skeleton, Field, Modal, Config } from "@oruga-ui/oruga-next";
 import { bulmaConfig } from "@oruga-ui/theme-bulma";
 
 export const install = (app: App) => {
-  app.use(Autocomplete).use(Modal).use(Field).use(Skeleton).use(Table).use(Config, bulmaConfig);
+  app.use(Autocomplete).use(Modal).use(Field).use(Skeleton).use(Config, bulmaConfig);
 };
