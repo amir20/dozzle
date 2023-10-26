8839f5811fee49a5d696a95dbcde50a12f909d74
Amir Raminfar <findamir@gmail.com>
2023-10-26 15:36:35 -0700

    removes unused code

--- assets/modules/router.ts
+++ assets/modules/router.ts
@@ -2,7 +2,6 @@ import { type App } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
 import pages from "~pages";
 import { setupLayouts } from "virtual:generated-layouts";
-import config from "@/stores/config";
 
 export const install = (app: App) => {
   const routes = setupLayouts(pages);
