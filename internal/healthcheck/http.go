84d08a373b8dff77679dc016e31a25f8d6247692
Amir Raminfar <findamir@gmail.com>
2023-11-17 16:23:44 -0800

    feat: adds logging for healthcheck

--- internal/healthcheck/http.go
+++ internal/healthcheck/http.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"path"
 	"strings"
 
 	log "github.com/sirupsen/logrus"
@@ -14,19 +15,22 @@ func HttpRequest(addr string, base string) error {
 		addr = "localhost" + addr
 	}
 
-	url := fmt.Sprintf("http://%s%s/healthcheck", addr, base)
+	url := fmt.Sprintf("http://"+path.Clean("%s%s/healthcheck"), addr, base)
 	log.Info("Checking health of " + url)
 	resp, err := http.Get(url)
 
 	if err != nil {
+		log.Error("Healthcheck failed with error: " + err.Error())
 		return err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode == 200 {
+		log.Info("Healthcheck passed")
 		os.Exit(0)
 	}
 
+	log.Errorf("Healthcheck failed with status code %d", resp.StatusCode)
 	os.Exit(1)
 
 	return nil
