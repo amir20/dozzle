94d82aadd4463db198875d096af12e61d38edb36
Amir Raminfar <findamir@gmail.com>
2023-12-01 06:08:54 -0800

    moves health icon

--- assets/components/LogViewer/ContainerTitle.vue
+++ assets/components/LogViewer/ContainerTitle.vue
@@ -5,7 +5,6 @@
       <carbon:star-filled class="swap-on text-secondary" />
       <carbon:star class="swap-off" />
     </label>
-    <container-health :health="container.health" v-if="container.health"></container-health>
     <div class="inline-flex font-mono text-sm">
       <div v-if="config.hosts.length > 1" class="mobile-hidden font-thin">
         {{ container.hostLabel }}<span class="mx-2">/</span>
@@ -18,7 +17,7 @@
         {{ container.swarmId }}
       </div>
     </div>
-
+    <container-health :health="container.health" v-if="container.health"></container-health>
     <tag class="mobile-hidden font-mono" size="small">{{ container.image.replace(/@sha.*/, "") }}</tag>
   </div>
 </template>
