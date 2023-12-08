e43024d8ba9188b1af95b325ccdfa85ebb681320
Akash R <developer.akash05@gmail.com>
2023-12-09 05:23:54 +0530

    Moved toast messages to locales

--- assets/components/LogViewer/CopyLogMessage.vue
+++ assets/components/LogViewer/CopyLogMessage.vue
@@ -1,11 +1,11 @@
 <template>
   <div
     class="absolute -right-1 flex min-w-[0.98rem] items-start justify-end align-bottom hover:cursor-pointer"
-    v-if="message.trim() != ''"
+    v-if="isSupported && message.trim() != ''"
     title="Copy Log"
   >
     <span
-      class="rounded bg-slate-800/60 px-1.5 py-1 text-primary opacity-0 transition-opacity delay-500 duration-1000 hover:bg-slate-700 group-hover/entry:opacity-100"
+      class="duration-250 rounded bg-slate-800/60 px-1.5 py-1 text-primary opacity-0 transition-opacity delay-100 hover:bg-slate-700 group-hover/entry:opacity-100"
       @click="copyLogMessageToClipBoard()"
     >
       <carbon:copy-file />
@@ -19,17 +19,21 @@ const { message } = defineProps<{
 }>();
 
 const { showToast } = useToast();
+const { copy, isSupported, copied } = useClipboard();
+const { t } = useI18n();
 
 function copyLogMessageToClipBoard() {
-  navigator.clipboard.writeText(message);
-
-  showToast(
-    {
-      title: "Copied",
-      message: "Log message copied to clipboard",
-      type: "info",
-    },
-    { expire: 2000 },
-  );
+  copy(message).then(() => {
+    if (copied.value) {
+      showToast(
+        {
+          title: t("toasts.copied.title"),
+          message: t("toasts.copied.message"),
+          type: "info",
+        },
+        { expire: 2000 },
+      );
+    }
+  });
 }
 </script>
