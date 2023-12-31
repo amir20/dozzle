4bd66601cbdb7ba16ab0baeb0c7da93b4639eb20
Amir Raminfar <findamir@gmail.com>
2023-12-30 18:53:00 -0800

    docs: removes custom content link in docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -69,7 +69,6 @@ export default defineConfig({
           { text: "Hostname", link: "/guide/hostname" },
           { text: "Remote Hosts", link: "/guide/remote-hosts" },
           { text: "Container Actions", link: "/guide/actions" },
-          { text: "Custom Documentation", link: "/guide/custom-documentation" },
           { text: "Supported Env Vars", link: "/guide/supported-env-vars" },
           { text: "Logging Files on Disk", link: "/guide/log-files-on-disk" },
           { text: "Analytics", link: "/guide/analytics" },
