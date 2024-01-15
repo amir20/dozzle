39acf0eda4803587cde47bfa0d41967f857bd01c
Amir Raminfar <findamir@gmail.com>
2024-01-15 08:39:07 -0800

    feat: adds option to home page to show more than default containers per page

--- assets/components/ContainerTable.vue
+++ assets/components/ContainerTable.vue
@@ -1,5 +1,14 @@
 !
 <template>
+  <div class="text-right">
+    Show per page
+    <dropdown-menu
+      class="dropdown-left"
+      v-model="perPage"
+      :options="pageSizes.map((i) => ({ label: i.toLocaleString(), value: i }))"
+      v-if="containers.length > 0"
+    />
+  </div>
   <table class="table table-lg bg-base">
     <thead>
       <tr :data-direction="direction > 0 ? 'asc' : 'desc'">
@@ -95,11 +104,13 @@ const fields = {
   },
 };
 
-const { containers, perPage = 15 } = defineProps<{
+const { containers } = defineProps<{
   containers: Container[];
-  perPage?: number;
 }>();
 type keys = keyof typeof fields;
+
+const perPage = ref(15);
+const pageSizes = [15, 30, 50, 100];
 
 const storage = useStorage<{ column: keys; direction: 1 | -1 }>("DOZZLE_TABLE_CONTAINERS_SORT", {
   column: "created",
@@ -115,12 +126,12 @@ const sortedContainers = computedWithControl(
   },
 );
 
-const totalPages = computed(() => Math.ceil(sortedContainers.value.length / perPage));
+const totalPages = computed(() => Math.ceil(sortedContainers.value.length / perPage.value));
 const isPaginated = computed(() => totalPages.value > 1);
 const currentPage = ref(1);
 const paginated = computed(() => {
-  const start = (currentPage.value - 1) * perPage;
-  const end = start + perPage;
+  const start = (currentPage.value - 1) * perPage.value;
+  const end = start + perPage.value;
 
   return sortedContainers.value.slice(start, end);
 });
--- assets/components/common/DropdownMenu.vue
+++ assets/components/common/DropdownMenu.vue
@@ -1,9 +1,9 @@
 <template>
   <details class="dropdown" ref="details" v-on-click-outside="close">
     <summary class="btn btn-primary flex-nowrap" v-bind="$attrs">
-      <slot name="trigger"> {{ values[modelValue] ?? defaultLabel }} <carbon:caret-down /></slot>
+      <slot name="trigger"> {{ label }} <carbon:caret-down /></slot>
     </summary>
-    <ul class="menu dropdown-content rounded-box z-50 mt-1 w-52 border border-base-content/20 bg-base p-2 shadow">
+    <ul class="menu dropdown-content z-50 mt-1 w-52 rounded-box border border-base-content/20 bg-base p-2 shadow">
       <slot>
         <li v-for="item in options">
           <a @click="modelValue = item.value">
@@ -17,27 +17,18 @@
   </details>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" setup generic="T">
 import { vOnClickOutside } from "@vueuse/components";
 type DropdownItem = {
   label: string;
-  value: string;
+  value: T;
 };
 const { options = [], defaultLabel = "" } = defineProps<{ options?: DropdownItem[]; defaultLabel?: string }>();
 const { modelValue } = defineModels<{
-  modelValue: string;
+  modelValue: T;
 }>();
 
-const values = computed(() =>
-  options.reduce(
-    (acc, curr) => {
-      acc[curr.value] = curr.label;
-      return acc;
-    },
-    {} as Record<string, string>,
-  ),
-);
-
+const label = computed(() => options.find((item) => item.value === modelValue.value)?.label ?? defaultLabel);
 const details = ref<HTMLElement | null>(null);
 const close = () => details.value?.removeAttribute("open");
 watch(modelValue, () => close());
