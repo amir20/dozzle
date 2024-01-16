98556f3369f97adedfc9eb226fc36b9837770141
Amir Raminfar <findamir@gmail.com>
2024-01-16 13:42:29 -0800

    feat: adds support for LogFmt. fixes #2695

--- internal/docker/event_generator.go
+++ internal/docker/event_generator.go
@@ -13,6 +13,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/go-logfmt/logfmt"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -175,6 +176,17 @@ func createEvent(message string, streamType StdType) *LogEvent {
 						}
 					}
 				} else {
+					logEvent.Message = data
+				}
+			} else {
+				decoder := logfmt.NewDecoder(strings.NewReader(message))
+				data := make(map[string]string)
+				for decoder.ScanKeyval() {
+					key := decoder.Key()
+					value := decoder.Value()
+					data[string(key)] = string(value)
+				}
+				if len(data) > 0 {
 					logEvent.Message = data
 				}
 			}
