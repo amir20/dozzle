a96ee0932d5f0124a7ee7f66daf3616e132bbbbf
Amir Raminfar <findamir@gmail.com>
2024-05-16 16:14:30 -0700

    adds missing file

---
+++ assets/components/LogViewer/ContainerName.vue
@@ -0,0 +1,25 @@
+<template>
+  <Tag size="small" class="random-color !block w-40 truncate">
+    {{ containerNames[id] }}
+  </Tag>
+</template>
+<script lang="ts" setup>
+const containerStore = useContainerStore();
+const { containerNames } = storeToRefs(containerStore);
+
+const { id } = defineProps<{
+  id: string;
+}>();
+
+const colors = ["#FF0000", "#FF7F00", "#FFFF00", "#00FF00", "#00FFFF", "#0000FF", "#FF00FF", "#FF007F"];
+
+const color = computed(() => {
+  return colors[id.charCodeAt(0) % colors.length];
+});
+</script>
+
+<style lang="postcss" scoped>
+.random-color {
+  background-color: v-bind(color);
+}
+</style>
