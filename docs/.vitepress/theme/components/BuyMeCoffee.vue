a089b394e060161e74507aa7334ab88ba924a42a
Amir Raminfar <findamir@gmail.com>
2024-05-24 10:37:10 -0700

    chore: updates buy me button

--- docs/.vitepress/theme/components/BuyMeCoffee.vue
+++ docs/.vitepress/theme/components/BuyMeCoffee.vue
@@ -2,12 +2,9 @@
 
 <template>
   <div class="content">
-    <a href="https://www.buymeacoffee.com/amirraminfar" target="_blank" rel="noopener noreferrer">
-      <img
-        src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
-        alt="Buy Me A Coffee"
-        style="height: 60px !important; width: 217px !important"
-      />
-    </a>
+    <a href="https://www.buymeacoffee.com/amirraminfar"
+      ><img
+        src="https://img.buymeacoffee.com/button-api/?text=Buy me a beer&emoji=🍺&slug=amirraminfar&button_colour=FFDD00&font_colour=000000&font_family=Poppins&outline_colour=000000&coffee_colour=ffffff"
+    /></a>
   </div>
 </template>
