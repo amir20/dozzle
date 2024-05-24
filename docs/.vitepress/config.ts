555c0b722224399652b491ba030a4f9e23e7608f
Amir Raminfar <findamir@gmail.com>
2024-05-24 10:08:30 -0700

    docs: generates better sitemap

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -108,24 +108,7 @@ export default defineConfig({
     ],
   },
 
-  transformHtml: (_, id, { pageData }) => {
-    if (!/[\\/]404\.html$/.test(id))
-      links.push({
-        // you might need to change this if not using clean urls mode
-        url: pageData.relativePath.replace(/((^|\/)index)?\.md$/, "$2"),
-        lastmod: pageData.lastUpdated,
-      });
-  },
-
-  buildEnd: async ({ outDir }) => {
-    const sitemap = new SitemapStream({
-      hostname: "https://dozzle.dev/",
-    });
-    const writeStream = createWriteStream(resolve(outDir, "sitemap.xml"));
-    sitemap.pipe(writeStream);
-    links.forEach((link) => sitemap.write(link));
-    const promise = streamToPromise(sitemap);
-    sitemap.end();
-    await promise;
+  sitemap: {
+    hostname: "https://dozzle.dev/",
   },
 });
