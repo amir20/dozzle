e9f2fe8a02e9b922563a4f20b48134faa3b86b5c
Amir Raminfar <findamir@gmail.com>
2024-06-02 07:51:23 -0700

    docs: updates video

--- docs/.vitepress/theme/components/HeroVideo.vue
+++ docs/.vitepress/theme/components/HeroVideo.vue
@@ -27,18 +27,10 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <browser-window drop-shadow-md>
-    <video muted loop autoplay playsinline poster="../media/poster.png" v-if="isDark">
-      <source src="../media/dozzle-dark.webm" type="video/webm" />
-      <source src="../media/dozzle-dark.mp4" type="video/mp4" />
-      <img src="../media/poster.png" alt="" />
-    </video>
-    <video muted loop autoplay playsinline poster="../media/poster.png" v-else>
-      <source src="../media/dozzle-light.webm" type="video/webm" />
-      <source src="../media/dozzle-light.mp4" type="video/mp4" />
-      <img src="../media/poster.png" alt="" />
-    </video>
-  </browser-window>
+  <video muted loop autoplay playsinline poster="../media/poster.png" v-if="isDark" class="drop-shadow-md">
+    <source src="../media/dozzle-dark.mp4" type="video/mp4" />
+    <img src="../media/poster.png" alt="" />
+  </video>
 </template>
 
 <style scoped></style>
