214fe49cfed4b4b0e7a4ed592f6593e7dc6b1d74
Amir Raminfar <findamir@gmail.com>
2024-06-26 17:41:48 -0700

    updates makefile

--- generate_certs.sh
+++
@@ -1,3 +0,0 @@
-openssl genpkey -algorithm RSA -out shared_key.pem -pkeyopt rsa_keygen_bits:2048
-openssl req -new -key shared_key.pem -out shared_request.csr -subj "/C=US/ST=California/L=San Francisco/O=Dozzle"
-openssl x509 -req -in shared_request.csr -signkey shared_key.pem -out shared_cert.pem -days 365
