65907ba86a31fe5591729a82ef7e566a93d36f15
Amir Raminfar <findamir@gmail.com>
2024-08-12 14:05:32 -0700

    feat: reads certs locally if available

--- internal/support/cli/certs.go
+++ internal/support/cli/certs.go
@@ -3,9 +3,16 @@ package cli
 import (
 	"crypto/tls"
 	"embed"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func ReadCertificates(certs embed.FS) (tls.Certificate, error) {
+	if pair, err := tls.LoadX509KeyPair("dozzle_cert.pem", "dozzle_key.pem"); err == nil {
+		log.Infof("using dozzle certificate and key at ./dozzle_cert.pem and ./dozzle_key.pem")
+		return pair, nil
+	}
+
 	cert, err := certs.ReadFile("shared_cert.pem")
 	if err != nil {
 		return tls.Certificate{}, err
