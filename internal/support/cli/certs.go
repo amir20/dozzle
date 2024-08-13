59e6608d45845026106495618c53e0ad8d4fcd1d
Amir Raminfar <findamir@gmail.com>
2024-08-13 10:21:30 -0700

    adds more logging

--- internal/support/cli/certs.go
+++ internal/support/cli/certs.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"crypto/tls"
 	"embed"
+	"os"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -11,6 +12,11 @@ func ReadCertificates(certs embed.FS) (tls.Certificate, error) {
 	if pair, err := tls.LoadX509KeyPair("dozzle_cert.pem", "dozzle_key.pem"); err == nil {
 		log.Infof("Found dozzle certificate and key at ./dozzle_cert.pem and ./dozzle_key.pem")
 		return pair, nil
+	} else {
+		if !os.IsNotExist(err) {
+			log.Errorf("Failed to load dozzle certificate and key: %v", err)
+			log.Warnf("Falling back to shared certificate and key")
+		}
 	}
 
 	cert, err := certs.ReadFile("shared_cert.pem")
