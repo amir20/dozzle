1050d99ad4cb3763e062b5aacb74d115dd072737
Amir Raminfar <findamir@gmail.com>
2024-08-26 10:00:11 -0700

    WIP

--- assets/components/LogViewer/LogList.vue
+++ assets/components/LogViewer/LogList.vue
@@ -1,5 +1,6 @@
 <template>
   <ul
+    ref="list"
     class="events group py-4"
     :class="{ 'disable-wrap': !softWrap, [size]: true, compact }"
     v-if="messages.length > 0"
@@ -36,6 +37,56 @@ const { messages } = defineProps<{
 watchEffect(() => {
   loading.value = messages.length === 0;
 });
+const list = ref<HTMLElement>();
+
+useMutationObserver(
+  list,
+  (mutations) => {
+    for (const mutation of mutations) {
+      if (mutation.type === "childList") {
+        const addedNodes = Array.from(mutation.addedNodes);
+        for (const node of addedNodes) {
+          if (node instanceof HTMLElement) {
+            observer.observe(node);
+          }
+        }
+
+        const removedNodes = Array.from(mutation.removedNodes);
+        for (const node of removedNodes) {
+          if (node instanceof HTMLElement) {
+            observer.unobserve(node);
+          }
+        }
+      }
+    }
+  },
+  {
+    childList: true,
+  },
+);
+
+watchOnce(
+  () => messages.length,
+  () =>
+    nextTick(() => {
+      if (list.value) {
+        Array.from(list.value.children).forEach((child) => observer.observe(child));
+      }
+    }),
+);
+const observer = new IntersectionObserver(
+  (entries) => {
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        console.log("intersecting", entry);
+      }
+    }
+  },
+  {
+    rootMargin: "0px 0px -90% 0px",
+    threshold: 1,
+  },
+);
 </script>
 <style scoped lang="postcss">
 .events {
