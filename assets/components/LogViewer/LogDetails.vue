db33e281176059af9c49dd52595e19b2b1a4947e
Amir Raminfar <findamir@gmail.com>
2024-08-29 13:11:22 -0700

    chore: fixes wrap

--- assets/components/LogViewer/LogDetails.vue
+++ assets/components/LogViewer/LogDetails.vue
@@ -37,7 +37,7 @@
       </thead>
       <tbody ref="list">
         <tr v-for="{ key, value, enabled } in fields" :key="key.join('.')" class="hover">
-          <td class="cursor-move font-mono">
+          <td class="cursor-move break-all font-mono">
             {{ key.join(".") }}
           </td>
           <td class="truncate">
