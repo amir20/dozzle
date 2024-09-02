27068a491bfd1455baa02fe16ff52b4f02772f91
Amir Raminfar <findamir@gmail.com>
2024-09-02 12:59:54 -0700

    fixes tests

--- internal/docker/level_guesser_test.go
+++ internal/docker/level_guesser_test.go
@@ -29,10 +29,6 @@ func TestGuessLogLevel(t *testing.T) {
 		{"[foo] [ ERROR] Something went wrong", "error"},
 		{"123 ERROR Something went wrong", "error"},
 		{"123 Something went wrong", ""},
-		{map[string]interface{}{"level": "info"}, "info"},
-		{map[string]interface{}{"level": "INFO"}, "info"},
-		{map[string]string{"level": "info"}, "info"},
-		{map[string]string{"level": "INFO"}, "info"},
 		{orderedmap.New[string, string](
 			orderedmap.WithInitialData(
 				orderedmap.Pair[string, string]{Key: "key", Value: "value"},
