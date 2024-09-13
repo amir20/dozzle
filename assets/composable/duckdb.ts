c617106a5158527b78b9a33fbb213d40a4bc6e05
Amir Raminfar <findamir@gmail.com>
2024-09-13 16:33:24 -0700

    cleans up

--- assets/composable/duckdb.ts
+++ assets/composable/duckdb.ts
@@ -1,22 +1,30 @@
 import * as duckdb from "@duckdb/duckdb-wasm";
+import duckdb_wasm from "@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url";
+import mvp_worker from "@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?url";
+import duckdb_wasm_eh from "@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url";
+import eh_worker from "@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url";
 
-const JSDELIVR_BUNDLES = duckdb.getJsDelivrBundles();
-
-const bundle = await duckdb.selectBundle(JSDELIVR_BUNDLES);
+const MANUAL_BUNDLES: duckdb.DuckDBBundles = {
+  mvp: {
+    mainModule: duckdb_wasm,
+    mainWorker: mvp_worker,
+  },
+  eh: {
+    mainModule: duckdb_wasm_eh,
+    mainWorker: eh_worker,
+  },
+};
+const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
 
 export async function useDuckDB() {
   let cleanup: (() => void) | undefined;
   onUnmounted(() => cleanup?.());
 
-  const worker_url = URL.createObjectURL(
-    new Blob([`importScripts("${bundle.mainWorker!}");`], { type: "text/javascript" }),
-  );
-
-  const worker = new Worker(worker_url);
+  const worker = new Worker(bundle.mainWorker!);
   const logger = new duckdb.ConsoleLogger();
   const db = new duckdb.AsyncDuckDB(logger, worker);
+
   await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
-  URL.revokeObjectURL(worker_url);
   const conn = await db.connect();
 
   cleanup = async () => {
--- internal/web/csp.go
+++ internal/web/csp.go
@@ -6,7 +6,7 @@ import (
 
 func cspHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;")
+		// w.Header().Set("Content-Security-Policy", "default-src 'self' 'wasm-unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; script-src 'self' 'wasm-unsafe-eval';")
 		next.ServeHTTP(w, r)
 	})
 }
