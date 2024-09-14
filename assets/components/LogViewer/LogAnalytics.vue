e82e3e627d42e9cd25775d169afacee7a537b689
Amir Raminfar <findamir@gmail.com>
2024-09-14 13:14:45 -0700

    adds query textarea

--- assets/components/LogViewer/LogAnalytics.vue
+++ assets/components/LogViewer/LogAnalytics.vue
@@ -5,7 +5,11 @@
       <h2 class="text-sm"><DistanceTime :date="container.created" /></h2>
     </header>
 
-    <section>Total {{ table.numRows }} records</section>
+    <section>
+      <textarea v-model="query" class="textarea textarea-primary w-full"></textarea>
+    </section>
+
+    <section>Total {{ results.numRows }} records</section>
 
     <table class="table table-zebra table-pin-rows table-md">
       <thead>
@@ -25,28 +29,36 @@
 <script setup lang="ts">
 import { Container } from "@/models/Container";
 const { container } = defineProps<{ container: Container }>();
+const query = ref("SELECT * FROM logs");
+const debouncedQuery = debouncedRef(query, 1000);
+
 const url = withBase(
   `/api/hosts/${container.host}/containers/${container.id}/logs?stdout=1&stderr=1&everything&jsonOnly`,
 );
-const { db, conn } = await useDuckDB();
 
 const response = await fetch(url);
-
 if (!response.ok) {
   console.log("error fetching logs from", url);
   throw new Error(`Failed to fetch logs: ${response.statusText}`);
 }
 
+const { db, conn } = await useDuckDB();
+
 await db.registerFileBuffer("logs.json", new Uint8Array(await response.arrayBuffer()));
 
 await conn.query(`CREATE TABLE logs AS SELECT unnest(m) FROM 'logs.json'`);
 
-const results = await conn.query<Record<string, any>>(`SELECT * FROM logs`);
+const results = computedAsync(
+  async () => await conn.query<Record<string, any>>(debouncedQuery.value),
+  { numRows: 0 },
+  {
+    onError: (error) => console.error(error),
+  },
+);
 
-const table = ref(results);
-const rows = shallowRef(results.toArray().map((row: Record<string, any>) => ({ ...row })));
-const columns = computed(() => (rows ? Object.keys(rows.value[0]) : []));
-
-const page = computed(() => table.value.slice(0, 20));
+const columns = computed(() =>
+  results.value.numRows > 0 ? Object.keys(results.value.get(0) as Record<string, any>) : [],
+);
+const page = computed(() => (results.value.numRows > 0 ? results.value.slice(0, 20) : []));
 </script>
 <style lang="postcss" scoped></style>
