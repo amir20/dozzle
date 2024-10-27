c6339effd1563bdf67b7a51391b724d9a382badc
Amir Raminfar <findamir@gmail.com>
2024-10-27 13:37:02 -0700

    fix: fix base ANSI convertor colors (#3353)
    Co-authored-by: Jérémy Touati <sielver@gmail.com>

--- assets/components/LogViewer/SimpleLogItem.vue
+++ assets/components/LogViewer/SimpleLogItem.vue
@@ -23,8 +23,8 @@ import stripAnsi from "strip-ansi";
 
 const ansiConvertor = new AnsiConvertor({
   escapeXML: false,
-  fg: "var(--base-content-color)",
-  bg: "var(--base-color)",
+  fg: "oklch(var(--base-content-color))",
+  bg: "oklch(var(--base-color))",
 });
 
 const { showContainerName = false } = defineProps<{
