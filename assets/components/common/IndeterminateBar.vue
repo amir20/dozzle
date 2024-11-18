c795bc1004b30ff687206f917de0bb779a016e68
Amir Raminfar <findamir@gmail.com>
2024-11-18 10:00:27 -0800

    adds missing file

---
+++ assets/components/common/IndeterminateBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="animate-background h-1 w-1/2 bg-gradient-radial to-transparent to-75%" :class="colorClass"></div>
+</template>
+<script setup lang="ts">
+const { color = "primary" } = defineProps<{ color: "primary" | "error" | "secondary" }>();
+
+const colorClass = computed(() => {
+  switch (color) {
+    case "primary":
+      return "from-primary";
+    case "error":
+      return "from-error";
+    case "secondary":
+      return "from-secondary";
+  }
+});
+</script>
+
+<style scoped lang="postcss">
+.animate-background {
+  animation: gradient-animation 3s ease-out infinite;
+}
+
+@keyframes gradient-animation {
+  0%,
+  100% {
+    transform: translateX(0%);
+  }
+  50% {
+    transform: translateX(100%);
+  }
+}
+</style>
