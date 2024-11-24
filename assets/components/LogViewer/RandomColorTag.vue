c36cf72506ae26085d203349857f22b7c7347842
Amir Raminfar <findamir@gmail.com>
2024-11-24 09:07:04 -0800

    feat: improves colors for containers

--- assets/components/LogViewer/RandomColorTag.vue
+++ assets/components/LogViewer/RandomColorTag.vue
@@ -6,21 +6,26 @@
 </template>
 <script lang="ts">
 const colors = [
-  "#4B0082",
-  "#FF00FF",
-  "#FF7F00",
-  "#FFFF00",
-  "#00FF00",
-  "#00FFFF",
-  "#FF0000",
-  "#0000FF",
-  "#FF007F",
-  "#32CD32",
-  "#40E0D0",
-  "#E6E6FA",
-  "#800080",
-  "#FFD700",
-  "#FF4040",
+  "hsl(200, 85%, 65%)", // Vibrant Sky Blue
+  "hsl(150, 85%, 65%)", // Vibrant Mint
+  "hsl(300, 85%, 65%)", // Vibrant Purple
+  "hsl(25, 85%, 65%)", // Vibrant Peach
+  "hsl(270, 85%, 65%)", // Vibrant Lavender
+  "hsl(340, 85%, 65%)", // Vibrant Rose
+  "hsl(170, 85%, 65%)", // Vibrant Aqua
+  "hsl(50, 85%, 65%)", // Vibrant Yellow
+  "hsl(235, 85%, 65%)", // Vibrant Periwinkle
+  "hsl(10, 85%, 65%)", // Vibrant Coral
+  "hsl(180, 85%, 65%)", // Vibrant Turquoise
+  "hsl(320, 85%, 65%)", // Vibrant Orchid
+  "hsl(90, 85%, 65%)", // Vibrant Lime
+  "hsl(260, 85%, 65%)", // Vibrant Amethyst
+  "hsl(30, 85%, 65%)", // Vibrant Orange
+  "hsl(210, 85%, 65%)", // Vibrant Ocean
+  "hsl(290, 85%, 65%)", // Vibrant Mauve
+  "hsl(120, 85%, 65%)", // Vibrant Spring
+  "hsl(350, 85%, 65%)", // Vibrant Strawberry
+  "hsl(190, 85%, 65%)", // Vibrant Azure
 ] as const;
 </script>
 <script lang="ts" setup>
