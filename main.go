f00698db658a30b0ae22c8bf6ebe9e441c53cf56
Amir Raminfar <findamir@gmail.com>
2024-12-16 16:54:27 -0800

    chore: uses air instead of reflex

--- main.go
+++ main.go
@@ -209,7 +209,7 @@ func main() {
 			log.Fatal().Err(err).Msg("failed to listen")
 		}
 	}()
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
 	defer stop()
 
 	<-ctx.Done()
