d291d6ac0f1b46d7994f7190cd73e678aeb74238
Amir Raminfar <findamir@gmail.com>
2025-01-04 14:28:41 -0800

    more fixes

--- assets/components/common/Tag.vue
+++ assets/components/common/Tag.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="tag bg-base-100 inline-flex items-center justify-center rounded-sm px-2 py-[0.2em] [[size='small']]:text-sm"
+    class="tag bg-base-100 inline-flex items-center justify-center rounded-sm px-2 py-[0.2em] [[size='small']]:text-[0.8rem]"
     :size="size"
   >
     <slot></slot>
