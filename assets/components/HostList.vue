f896990c124642c2d509462f271af080e00ab5fe
Amir Raminfar <findamir@gmail.com>
2025-01-04 19:03:21 -0800

    tries to fix progress

--- assets/components/HostList.vue
+++ assets/components/HostList.vue
@@ -50,7 +50,7 @@
             :style="`--value: ${(weightedStats[host.id].weighted.totalMem / host.memTotal) * 100};`"
             role="progressbar"
           >
-            <span class="whitespace-nowrap">{{ formatBytes(weightedStats[host.id].weighted.totalMem, 1) }}</span>
+            {{ weightedStats[host.id].weighted.totalCPU.toFixed(0) }}%
           </div>
         </div>
       </div>
