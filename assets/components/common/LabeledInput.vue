81f24d1c26f74d0b1f4f6562e6039c5ec465271f
Amir Raminfar <findamir@gmail.com>
2025-01-06 14:49:34 -0800

    chore: fix color

--- assets/components/common/LabeledInput.vue
+++ assets/components/common/LabeledInput.vue
@@ -1,5 +1,5 @@
 <template>
-  <label class="label cursor-pointer gap-4">
+  <label class="label text-base-content cursor-pointer gap-4">
     <div class="flex-1"><slot name="label" /></div>
     <slot name="input" />
   </label>
