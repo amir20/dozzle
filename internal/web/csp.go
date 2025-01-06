42cc254eb7654310c5925a5a3c30ce4507f63ce2
Amir Raminfar <findamir@gmail.com>
2025-01-06 13:19:24 -0800

    chore: fixes CSP

--- internal/web/csp.go
+++ internal/web/csp.go
@@ -8,7 +8,7 @@ func cspHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set(
 			"Content-Security-Policy",
-			"default-src 'self' 'wasm-unsafe-eval' blob: https://cdn.jsdelivr.net https://*.duckdb.org; style-src 'self' 'unsafe-inline'; img-src 'self' data:;",
+			"default-src 'self' 'wasm-unsafe-eval' blob: https://cdn.jsdelivr.net https://*.duckdb.org; style-src 'self' 'unsafe-inline' blob:; img-src 'self' data:;",
 		)
 		next.ServeHTTP(w, r)
 	})
