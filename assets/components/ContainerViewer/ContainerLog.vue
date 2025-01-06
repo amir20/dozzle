917c837e90ed3aac3e8775504939fdb95f3970b3
Amir Raminfar <findamir@gmail.com>
2025-01-06 12:57:20 -0800

    feat: adds a new drop down to jump to same name containers (#3510)

--- assets/components/ContainerViewer/ContainerLog.vue
+++ assets/components/ContainerViewer/ContainerLog.vue
@@ -3,7 +3,11 @@
     <template #header v-if="showTitle">
       <div class="@container mx-2 flex items-center gap-2 md:ml-4">
         <ContainerTitle :container="container" />
-        <MultiContainerStat class="ml-auto lg:hidden lg:@3xl:flex" :containers="[container]" />
+        <MultiContainerStat
+          class="ml-auto lg:hidden lg:@3xl:flex"
+          :containers="[container]"
+          v-if="container.state === 'running'"
+        />
 
         <ContainerActionsToolbar @clear="viewer?.clear()" class="mobile-hidden" :container="container" />
         <a class="btn btn-circle btn-xs" @click="close()" v-if="closable">
--- assets/components/ContainerViewer/ContainerTitle.vue
+++ assets/components/ContainerViewer/ContainerTitle.vue
@@ -1,20 +1,38 @@
 <template>
-  <div class="@container flex flex-1 gap-1.5 truncate md:gap-2">
+  <div class="@container flex flex-1 items-center gap-1.5 truncate md:gap-2">
     <label class="swap swap-rotate size-4">
       <input type="checkbox" v-model="pinned" />
       <carbon:star-filled class="swap-on text-secondary" />
       <carbon:star class="swap-off" />
     </label>
-    <div class="inline-flex font-mono text-sm">
-      <div v-if="config.hosts.length > 1" class="mobile-hidden font-thin">
-        {{ container.hostLabel }}<span class="mx-2">/</span>
-      </div>
-      <div class="font-semibold">{{ container.name }}</div>
-      <div
-        class="mobile-hidden max-w-[1.5em] truncate transition-[max-width] hover:max-w-[400px]"
-        v-if="container.isSwarm"
-      >
-        .{{ container.swarmId }}
+    <div class="inline-flex items-center text-sm">
+      <div class="breadcrumbs p-0">
+        <ul>
+          <li v-if="config.hosts.length > 1" class="mobile-hidden font-thin">
+            {{ container.hostLabel }}
+          </li>
+          <li>
+            <div class="wrapper" ref="wrapper">
+              <button popovertarget="popover-container-list" class="btn btn-xs md:btn-sm anchor font-mono">
+                {{ container.name }} <carbon:caret-down />
+              </button>
+              <ul popover id="popover-container-list" class="dropdown menu rounded-box bg-base-100 tethered shadow-sm">
+                <li v-for="other in otherContainers">
+                  <router-link :to="{ name: '/container/[id]', params: { id: other.id } }">
+                    <div
+                      class="status data-[state=exited]:status-error data-[state=running]:status-success"
+                      :data-state="other.state"
+                    ></div>
+                    <div class="font-mono" v-if="other.isSwarm">{{ other.swarmId }}</div>
+                    <div class="font-mono" v-else>{{ other.name }}</div>
+                    <div v-if="other.state === 'running'">running</div>
+                    <DistanceTime :date="other.created" strict class="text-base-content/70 text-xs" v-else />
+                  </router-link>
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
     <ContainerHealth :health="container.health" v-if="container.health" />
@@ -38,4 +56,41 @@ const pinned = computed({
     }
   },
 });
+const store = useContainerStore();
+const { containers: allContainers } = storeToRefs(store);
+
+const otherContainers = computed(() =>
+  [...allContainers.value.filter((c) => c.name === container.name && c.id !== container.id)].sort(
+    (a, b) => +b.created - +a.created,
+  ),
+);
+const wrapper = useTemplateRef("wrapper");
+
+onMounted(async () => {
+  if (!("anchorName" in document.documentElement.style)) {
+    // @ts-ignore
+    const module = await import("@oddbird/css-anchor-positioning/fn");
+    // @ts-ignore
+    await module.default([wrapper.value]);
+  }
+});
 </script>
+
+<style scoped>
+/* https://github.com/oddbird/css-anchor-positioning/issues/282 */
+.wrapper {
+  anchor-scope: --anchor;
+}
+
+.anchor {
+  anchor-name: --anchor;
+}
+
+.tethered {
+  margin: 0;
+  padding: 0;
+  position-anchor: --anchor;
+  top: anchor(bottom);
+  left: anchor(left);
+}
+</style>
