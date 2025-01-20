216e495631052078d61302d91dfb376227a6afac
Amir Raminfar <findamir@gmail.com>
2025-01-20 13:42:11 -0800

    fixes

--- docs/.vitepress/theme/components/Banner.vue
+++ docs/.vitepress/theme/components/Banner.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="banner fixed top-0 right-0 left-0 z-(--vp-z-index-layout-top) flex overflow-hidden bg-[#383636] p-2">
-    Thiis is a test
+  <div
+    class="banner fixed top-0 right-0 left-0 z-(--vp-z-index-layout-top) flex items-center gap-4 overflow-hidden bg-[#383636] p-2"
+  >
+    Dozzle is collecting user feedback to improve the product. Please take a moment to fill out our survey. Thank you!
+    <a href="https://tally.so/r/wLv4g2" target="_blank" class="btn btn-sm btn-primary">Take the survey</a>
     <button class="btn btn-circle btn-xs ml-auto" @click="dismiss">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
         <path
@@ -11,17 +14,24 @@
   </div>
 </template>
 <script setup>
+import { onMounted } from "vue";
 const dismiss = () => {
   localStorage.setItem("banner-dismissed", (Date.now() + 8.64e7 * 1).toString());
   document.documentElement.classList.add("banner-dismissed");
 };
+
+onMounted(() => {
+  if (new Date(+localStorage.getItem("banner-dismissed")) > Date.now()) {
+    document.documentElement.classList.add("banner-dismissed");
+  }
+});
 </script>
 <style>
 .banner-dismissed {
   --vp-layout-top-height: 0px !important;
 }
 html {
-  --vp-layout-top-height: 88px;
+  --vp-layout-top-height: 96px;
 }
 @media (min-width: 375px) {
   html {
