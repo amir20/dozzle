d1fc9b750b5d2e6e69b8178940c9cca956f58fb7
Amir Raminfar <findamir@gmail.com>
2025-01-21 10:27:12 -0800

    chore: docs fixes x again

--- docs/.vitepress/theme/components/Banner.vue
+++ docs/.vitepress/theme/components/Banner.vue
@@ -9,7 +9,13 @@
         Take a quick survey 🙏🏼
       </a>
     </div>
-    <button class="btn btn-circle btn-xs ml-auto" @click="dismiss">x</button>
+    <button class="btn btn-circle btn-xs ml-auto" @click="dismiss">
+      <svg class="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512">
+        <polygon
+          points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"
+        />
+      </svg>
+    </button>
   </div>
 </template>
 <script setup>
