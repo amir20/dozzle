ad5c1c44528829e8e2e795f9d312c2f20a04b55c
Amir Raminfar <findamir@gmail.com>
2025-01-23 11:23:34 -0800

    fix(docs): fixes label

--- docs/.vitepress/theme/components/Stats.vue
+++ docs/.vitepress/theme/components/Stats.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="stats mt-10">
+  <div class="stats stats-horizontal mt-10">
     <div class="stat w-40 border-0!">
-      <div class="stat-title">Github Stars</div>
+      <div class="stat-title text-(--vp-c-text-1)">Github Stars</div>
       <div class="stat-value">
         <Counter :start="0" :end="data.stars" :duration="1000" :formatter="(value) => (value / 1e3).toFixed(1) + 'K'" />
       </div>
     </div>
 
     <div class="stat">
-      <div class="stat-title">Docker Pulls</div>
+      <div class="stat-title text-(--vp-c-text-1)">Docker Pulls</div>
       <div class="stat-value">
         <Counter :start="0" :end="data.pulls" :duration="1000" :formatter="(value) => (value / 1e6).toFixed(0) + 'M'" />
       </div>
