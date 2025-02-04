4fb1cb86237560178f1f71d862214ab1f496b211
Amir Raminfar <findamir@gmail.com>
2025-02-04 08:56:42 -0800

    adds host

--- assets/components/common/HostIcon.vue
+++ assets/components/common/HostIcon.vue
@@ -2,6 +2,7 @@
   <mdi:satellite-variant v-if="type == 'agent'" />
   <ph:globe-simple v-else-if="type == 'remote'" />
   <mdi:hexagon-multiple v-else-if="type == 'swarm'" />
+  <carbon:logo-kubernetes v-else-if="type == 'k8s'" />
   <ph:computer-tower v-else />
 </template>
 <script setup lang="ts">
