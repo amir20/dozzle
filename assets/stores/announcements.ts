571de4084ea02261573beb3ac7131c5d43d1822d
Amir Raminfar <findamir@gmail.com>
2025-02-11 09:57:36 -0800

    chore: removes survey

--- assets/stores/announcements.ts
+++ assets/stores/announcements.ts
@@ -14,21 +14,7 @@ type Announcement = {
 
 const { data: releases } = useFetch(withBase("/api/releases")).get().json<Announcement[]>();
 
-const otherAnnouncements = [
-  {
-    body: "I'd love to hear about your experience in this short survey to shape the future of Dozzle!",
-    createdAt: new Date("2025-01-22T18:00:00Z"),
-    htmlUrl: "https://tally.so/r/wLv4g2?ref=notification",
-    name: "Take survey!",
-    announcement: true,
-    tag: "survey-2025-01",
-    latest: true,
-    mentionsCount: 0,
-    features: 0,
-    bugFixes: 0,
-    breaking: 0,
-  },
-] as Announcement[];
+const otherAnnouncements = [] as Announcement[];
 
 const announcements = computed(() => {
   const newReleases =
