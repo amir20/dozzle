3e3bb02b5f6c30ec8ad677066d8292683273356b
Amir Raminfar <findamir@gmail.com>
2025-03-17 09:20:04 -0700

    chore: fixes code suggestions (#3708)

--- assets/composable/profileStorage.ts
+++ assets/composable/profileStorage.ts
@@ -29,7 +29,7 @@ export function useProfileStorage<K extends keyof Profile>(
     if (transformer) {
       storage.value = transformer.from(config.profile[key]);
     } else if (storage.value instanceof Set && config.profile[key] instanceof Array) {
-      storage.value = new Set([...(config.profile[key] as Iterable<any>)]) as unknown as NonNullable<Profile[K]>;
+      storage.value = new Set(config.profile[key] as Iterable<any>) as unknown as NonNullable<Profile[K]>;
     } else if (config.profile[key] instanceof Array) {
       storage.value = config.profile[key] as NonNullable<Profile[K]>;
     } else if (config.profile[key] instanceof Object) {
