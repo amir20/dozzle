e4d7e84c58a42616b642ce8c309dc56c342f7b49
Amir Raminfar <findamir@gmail.com>
2025-04-07 16:48:58 -0700

    wip

--- assets/components/LogViewer/StatMonitor.vue
+++ assets/components/LogViewer/StatMonitor.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="hover:text-secondary relative" @mouseenter="mouseOver = true" @mouseleave="mouseOver = false">
-    <div class="border-primary hidden overflow-hidden rounded-xs border px-px pt-1 pb-px md:flex">
+    <div class="border-primary overflow-hidden rounded-xs border px-px pt-1 pb-px max-md:hidden">
       <StatSparkline :data="data" @selected-point="onSelectedPoint" />
     </div>
     <div class="bg-base-200 inline-flex gap-1 rounded-sm p-px text-xs md:absolute md:-top-2 md:-left-0.5">
       <div class="font-light uppercase">{{ label }}</div>
       <div class="font-bold select-none">
         {{ mouseOver ? (selectedPoint?.value ?? selectedPoint?.y ?? statValue) : statValue }}
-        <template v-if="limit !== -1"> / {{ limit }} </template>
+        <span v-if="limit !== -1" class="max-md:hidden"> / {{ limit }} </span>
       </div>
     </div>
   </div>
