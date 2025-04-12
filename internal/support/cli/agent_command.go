b33ae4fa4ede60598830203e6d0b93ab1f25afa8
Amir Raminfar <findamir@gmail.com>
2025-04-12 13:44:21 -0700

    fix: Use /tmp instead of ./ to ensure that user permissions can be mounted correctly. (#3797)

--- internal/support/cli/agent_command.go
+++ internal/support/cli/agent_command.go
@@ -15,7 +15,6 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-
 type AgentCmd struct {
 	Addr string `arg:"--agent-addr,env:DOZZLE_AGENT_ADDR" default:":7007" help:"sets the host:port to bind for the agent"`
 }
@@ -37,11 +36,12 @@ func (a *AgentCmd) Run(args Args, embeddedCerts embed.FS) error {
 	if err != nil {
 		return fmt.Errorf("failed to listen: %w", err)
 	}
-	tempFile, err := os.CreateTemp("./", "agent-*.addr")
+	tempFile, err := os.CreateTemp("", "agent-*.addr")
 	if err != nil {
 		return fmt.Errorf("failed to create temp file: %w", err)
 	}
 	io.WriteString(tempFile, listener.Addr().String())
+	log.Debug().Str("file", tempFile.Name()).Msg("Created temp file")
 	go StartEvent(args, "", client, "agent")
 	server, err := agent.NewServer(client, certs, args.Version(), args.Filter)
 	if err != nil {
