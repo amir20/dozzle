5abb87bd9b65f97edf18dd0f3dc438b764b8dcda
Amir Raminfar <findamir@gmail.com>
2025-04-13 15:08:03 -0700

    chore: fixes center css

--- assets/components/StackViewer/StackLog.vue
+++ assets/components/StackViewer/StackLog.vue
@@ -2,7 +2,7 @@
   <ScrollableView :scrollable="scrollable" v-if="stack.name">
     <template #header>
       <div class="mx-2 flex items-center gap-2 md:ml-4">
-        <div class="@container flex flex-1 gap-1.5 truncate md:gap-2">
+        <div class="@container flex flex-1 items-center gap-1.5 truncate md:gap-2">
           <ph:stack />
           <div class="inline-flex font-mono text-sm">
             <div class="font-semibold">{{ stack.name }}</div>
