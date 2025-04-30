f9fabd9792338cf2a22090c5c94649ddee49dd86
Amir Raminfar <findamir@gmail.com>
2025-04-30 09:59:44 -0700

    minor improvements

--- assets/components/SidePanel.vue
+++ assets/components/SidePanel.vue
@@ -1,18 +1,16 @@
 <template>
   <aside class="fixed flex h-screen w-[inherit] flex-col gap-4 p-3" data-testid="navigation">
     <h1>
-      <router-link :to="{ name: '/' }" class="my-2 flex items-center gap-4 text-4xl font-thin">
-        <Logo class="h-16" />
+      <router-link :to="{ name: '/' }" class="flex items-center gap-4 text-4xl font-thin">
+        <Logo class="h-14" />
         Dozzle
       </router-link>
 
-      <small class="mb-4 block text-xs font-light" v-if="hostname">
-        {{ hostname }}
-      </small>
+      <small class="mt-4 block text-sm font-light" v-if="hostname">{{ hostname }}</small>
     </h1>
 
     <button
-      class="input input-sm hover:border-primary inline-flex w-auto cursor-pointer items-center gap-2 self-start font-light"
+      class="input input-sm hover:border-primary mt-2 inline-flex w-auto cursor-pointer items-center gap-2 self-start font-light"
       @click="$emit('search')"
       :title="$t('tooltip.search')"
       data-testid="search"
@@ -22,7 +20,7 @@
       <key-shortcut char="k" class="text-base-content/70"></key-shortcut>
     </button>
 
-    <SideMenu class="mt-2 flex-1" />
+    <SideMenu class="flex-1" />
   </aside>
 </template>
 
