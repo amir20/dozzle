b961b11499eacbf329c09d975348512a6ae613a3
Amir Raminfar <findamir@gmail.com>
2025-04-30 15:50:27 -0700

    feat: updates dozzle logos and favicons (#3858)

--- assets/components/SidePanel.vue
+++ assets/components/SidePanel.vue
@@ -1,17 +1,16 @@
 <template>
   <aside class="fixed flex h-screen w-[inherit] flex-col gap-4 p-3" data-testid="navigation">
     <h1>
-      <router-link :to="{ name: '/' }">
-        <LogoWithText class="logo [&_.secondary-fill]:fill-secondary [&_.content-fill]:fill-base-content h-16 w-40" />
+      <router-link :to="{ name: '/' }" class="flex w-full items-center gap-4 overflow-hidden text-4xl font-thin">
+        <Logo class="h-14 w-14 shrink-0" />
+        Dozzle
       </router-link>
 
-      <small class="mb-4 block text-xs font-light" v-if="hostname">
-        {{ hostname }}
-      </small>
+      <small class="mt-4 block text-sm font-light" v-if="hostname">{{ hostname }}</small>
     </h1>
 
     <button
-      class="input input-sm hover:border-primary inline-flex w-auto cursor-pointer items-center gap-2 self-start font-light"
+      class="input input-sm hover:border-primary mt-2 inline-flex w-auto cursor-pointer items-center gap-2 self-start font-light"
       @click="$emit('search')"
       :title="$t('tooltip.search')"
       data-testid="search"
@@ -21,12 +20,12 @@
       <key-shortcut char="k" class="text-base-content/70"></key-shortcut>
     </button>
 
-    <SideMenu class="mt-2 flex-1" />
+    <SideMenu class="flex-1" />
   </aside>
 </template>
 
 <script lang="ts" setup>
-import LogoWithText from "@/logo-text.svg";
+import Logo from "@/logo.svg";
 const { hostname } = config;
 </script>
 
