5c206b6de3e61f415b9ddf0cddd7ce72a35c33b6
Amir Raminfar <findamir@gmail.com>
2025-04-30 20:56:55 -0700

    chore: fix css bug (#3865)

--- assets/components/LogViewer/LogItem.vue
+++ assets/components/LogViewer/LogItem.vue
@@ -2,7 +2,7 @@
   <div class="relative flex w-full items-start gap-x-2 group-[.compact]:items-stretch">
     <LogStd :std="logEntry.std" class="shrink-0 select-none" v-if="showStd" />
 
-    <div class="flex gap-x-2 gap-y-1 group-[.compact]:gap-y-0 has-[>_*:nth-of-type(2)]:flex-col-reverse md:flex-row">
+    <div class="flex gap-x-2 gap-y-1 group-[.compact]:gap-y-0 has-[>_*:nth-of-type(2)]:flex-col-reverse md:flex-row!">
       <RandomColorTag class="w-30 shrink-0 select-none md:w-40" :value="host.name" v-if="showHostname" />
       <RandomColorTag
         class="w-30 shrink-0 select-none group-[.compact]:flex-1 md:w-40"
