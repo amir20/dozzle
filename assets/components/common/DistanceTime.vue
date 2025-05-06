a09e809da5f153159ea2ad9d4579677f1c51a3a9
Amir Raminfar <findamir@gmail.com>
2025-05-06 14:47:27 -0700

    clean up

--- assets/components/common/DistanceTime.vue
+++ assets/components/common/DistanceTime.vue
@@ -23,28 +23,26 @@ function updateFromNow() {
 useIntervalFn(updateFromNow, 30_000, { immediateCallback: true });
 
 function getRelativeTime(date: Date, locale: string | undefined): string {
-  console.log("Locale:", locale);
-  const now = new Date();
-  const diffInSeconds = (date.getTime() - now.getTime()) / 1000;
+  const diffInSeconds = (date.getTime() - new Date().getTime()) / 1000;
+  const rtf = new Intl.RelativeTimeFormat(locale, { numeric: "auto" });
 
-  const units: { unit: Intl.RelativeTimeFormatUnit; seconds: number }[] = [
-    { unit: "year", seconds: 31536000 },
-    { unit: "month", seconds: 2592000 },
-    { unit: "week", seconds: 604800 },
-    { unit: "day", seconds: 86400 },
-    { unit: "hour", seconds: 3600 },
-    { unit: "minute", seconds: 60 },
-    { unit: "second", seconds: 1 },
+  const units: [Intl.RelativeTimeFormatUnit, number][] = [
+    ["year", 31536000],
+    ["month", 2592000],
+    ["week", 604800],
+    ["day", 86400],
+    ["hour", 3600],
+    ["minute", 60],
+    ["second", 1],
   ];
 
-  for (const { unit, seconds } of units) {
+  for (const [unit, seconds] of units) {
     const value = Math.round(diffInSeconds / seconds);
     if (Math.abs(value) >= 1) {
-      const rtf = new Intl.RelativeTimeFormat(locale, { numeric: "auto" });
       return rtf.format(value, unit);
     }
   }
 
-  return new Intl.RelativeTimeFormat(locale, { numeric: "auto" }).format(0, "second");
+  return rtf.format(0, "second");
 }
 </script>
