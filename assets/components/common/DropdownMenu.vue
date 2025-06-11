3c2a2119d8c1235d150448c62b28ab50c045023b
Amir Raminfar <findamir@gmail.com>
2025-06-11 08:33:58 -0700

    fix: adds scoll to menu (#3966)

--- assets/components/common/DropdownMenu.vue
+++ assets/components/common/DropdownMenu.vue
@@ -4,7 +4,7 @@
       <slot name="trigger"> {{ label }} <carbon:caret-down /></slot>
     </summary>
     <ul
-      class="menu dropdown-content rounded-box border-base-content/20 bg-base-200 z-50 mt-1 w-52 border p-2 shadow-sm"
+      class="menu dropdown-content rounded-box border-base-content/20 bg-base-200 z-50 mt-1 max-h-72 w-48 flex-nowrap overflow-auto border p-2 shadow-sm"
     >
       <slot>
         <li v-for="item in options">
