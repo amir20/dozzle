18a87e181140eb499f51449a4b72fe0f09dccc99
Amir Raminfar <findamir@gmail.com>
2025-06-11 16:20:27 -0700

    clean up

--- assets/components/LogViewer/LogViewer.vue
+++ assets/components/LogViewer/LogViewer.vue
@@ -13,44 +13,6 @@ const props = defineProps<{
 const { messages, visibleKeys } = toRefs(props);
 
 const { filteredPayload } = useVisibleFilter(visibleKeys);
-const { debouncedSearchFilter, isSearching } = useSearchFilter();
-const { streamConfig } = useLoggingContext();
-
 const visibleMessages = filteredPayload(messages);
-const router = useRouter();
-
-// watchArray([debouncedSearchFilter, streamConfig], () => {
-//   console.log("Updating route...");
-//   const query = router.currentRoute.value.query;
-//   const hash = router.currentRoute.value.hash;
-//   if (isSearching.value) {
-//     console.log("Searching...");
-//     query.search = debouncedSearchFilter.value;
-//   } else {
-//     delete query.search;
-//   }
-
-//   if (!streamConfig.value.stderr) {
-//     query.stderr = streamConfig.value.stderr.toString();
-//   } else {
-//     delete query.stderr;
-//   }
-
-//   if (!streamConfig.value.stdout) {
-//     query.stdout = streamConfig.value.stdout.toString();
-//   } else {
-//     delete query.stdout;
-//   }
-
-//   console.log("Updating route...", query);
-//   router.push({
-//     query,
-//     hash,
-//     replace: true,
-//   });
-// });
-//
-
-// TODO
 </script>
 <style scoped></style>
