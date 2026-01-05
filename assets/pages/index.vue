2537e16b6cbdc117465c2237d825e424fda19794
Amir Raminfar <findamir@gmail.com>
2026-01-05 09:11:05 -0800

    chore: shows host count (#4319)

--- assets/pages/index.vue
+++ assets/pages/index.vue
@@ -2,7 +2,7 @@
   <PageWithLinks>
     <section>
       <div class="mb-4 flex items-center justify-between">
-        <h2 class="text-lg font-semibold">{{ $t("label.hosts") }}</h2>
+        <h2 class="text-lg font-semibold">{{ $t("label.host-count", { count: Object.keys(hosts).length }) }}</h2>
         <button @click="hostsCollapsed = !hostsCollapsed" class="btn btn-ghost btn-sm">
           <mdi:chevron-down :class="{ 'rotate-180': !hostsCollapsed }" class="transition-transform" />
         </button>
@@ -15,7 +15,7 @@
     <section>
       <div class="mb-2 flex items-center justify-between">
         <h2 class="text-lg font-semibold">
-          {{ $t("title.dashboard", { count: runningContainers.length }) }}
+          {{ $t("label.container", { count: runningContainers.length }) }}
         </h2>
         <button @click="containersCollapsed = !containersCollapsed" class="btn btn-ghost btn-sm">
           <mdi:chevron-down :class="{ 'rotate-180': !containersCollapsed }" class="transition-transform" />
@@ -32,6 +32,7 @@
 import { Container } from "@/models/Container";
 
 const { t } = useI18n();
+const { hosts } = useHosts();
 
 const containerStore = useContainerStore();
 const { containers, ready } = storeToRefs(containerStore) as unknown as {
