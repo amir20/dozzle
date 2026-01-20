4954a020f9d90e353c10af8e7852f4a6e957c885
Amir Raminfar <findamir@gmail.com>
2026-01-19 19:33:20 -0800

    chore: updates info to debug

--- internal/notification/log_listener.go
+++ internal/notification/log_listener.go
@@ -2,7 +2,9 @@ package notification
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"time"
 
 	"github.com/amir20/dozzle/internal/container"
@@ -120,7 +122,7 @@ func (l *ContainerLogListener) startListening(c container.Container, client cont
 
 	go func() {
 		log.Debug().Str("containerID", c.ID).Str("name", c.Name).Msg("Started listening to container")
-		if err := client.StreamLogs(streamCtx, c, time.Now(), container.STDALL, l.logChannel); err != nil {
+		if err := client.StreamLogs(streamCtx, c, time.Now(), container.STDALL, l.logChannel); err != nil && !errors.Is(err, io.EOF) {
 			log.Error().Err(err).Str("containerID", c.ID).Msg("Error streaming logs")
 		}
 	}()
